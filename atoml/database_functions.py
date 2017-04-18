8fc7b5a56a96abfc81de9eb78446257aa1187958
Paul C. Jennings <jennings.p.c@gmail.com>
2017-04-17 17:04:40 -0700

    Add some comments on how to use db functions.

--- atoml/database_functions.py
+++ atoml/database_functions.py
@@ -7,11 +7,12 @@ class DescriptorDatabase(object):
     """ Class to store sets of descriptors for a given atoms object assigned a
         unique ID.
 
-        db_name: str
+        Parameters
+        ----------
+        db_name : str
             Name of the sqlite database to connect. Default is
             descriptor_store.sqlite.
-
-        table: str
+        table : str
             Name of the table in which to store the descriptors. Default is
             Descriptors.
     """
@@ -20,7 +21,13 @@ class DescriptorDatabase(object):
         self.table = table
 
     def create_db(self, names):
-        """ Function to setup a database storing descriptors. """
+        """ Function to setup a database storing descriptors.
+
+            Parameters
+            ----------
+            names : list
+                List of heading names for features and targets.
+        """
         # Attach database
         conn = sqlite3.connect(os.getcwd() + '/' + self.db_name)
         cursor = conn.cursor()
@@ -33,8 +40,34 @@ class DescriptorDatabase(object):
         cursor.execute("""CREATE TABLE IF NOT EXISTS %(table)s (%(cstring)s)"""
                        % {'table': self.table, 'cstring': cstring})
 
+    def create_column(self, new_column):
+        """ Function to create a new column in the table. Will be full of None
+            values.
+
+            new_column : str
+                Name of new feature or target.
+        """
+        conn = sqlite3.connect(os.getcwd() + '/' + self.db_name)
+        cursor = conn.cursor()
+
+        for c in new_column:
+            cursor.execute("alter table %(table)s add column %(new_column)s \
+                           'float'" % {'table': self.table, 'new_column': c})
+            conn.commit()
+        cursor.close()
+
     def fill_db(self, descriptor_names, data):
-        """ Function to fill the descriptor database. """
+        """ Function to fill the descriptor database.
+
+            Parameters
+            ----------
+            descriptor_names : list
+                List of descriptor names for features and targets.
+            data : array
+                First row should contain string of UUIDs, thereafter array
+                should contain floats corresponding to the descriptor names
+                provided.
+        """
         conn = sqlite3.connect(os.getcwd() + '/' + self.db_name)
         cursor = conn.cursor()
 
@@ -48,7 +81,17 @@ class DescriptorDatabase(object):
         conn.commit()
 
     def update_descriptor(self, descriptor, new_data, unique_id):
-        """ Function to update a descriptor based on a given uuid. """
+        """ Function to update a descriptor based on a given uuid.
+
+            Parameters
+            ----------
+            descriptor : str
+                Name of descriptor to be updated.
+            new_data : float
+                New value to be entered into table.
+            unique_id : str
+                The UUID of the entry to be updated.
+        """
         conn = sqlite3.connect(os.getcwd() + '/' + self.db_name)
         cursor = conn.cursor()
 
@@ -62,7 +105,18 @@ class DescriptorDatabase(object):
         conn.commit()
 
     def query_db(self, unique_id=None, names=None):
-        """ return single row based on uuid or all rows. """
+        """ Return single row based on uuid or all rows.
+
+            Parameters
+            ----------
+            unique_id : str
+                If specified, the data corresponding to the given UUID will
+                be returned. If None, all rows will be returned.
+            names : list
+                If specified, only the data corresponding to the provided
+                feature/target names will be returned. If None, all columns
+                will be returned.
+        """
         if names is None:
             names = '*'
         else:
@@ -93,14 +147,3 @@ class DescriptorDatabase(object):
         cursor = conn.execute('select * from %s' % self.table)
 
         return [description[0] for description in cursor.description]
-
-    def create_column(self, new_column):
-        """ Function to create a new column in the table. """
-        conn = sqlite3.connect(os.getcwd() + '/' + self.db_name)
-        cursor = conn.cursor()
-
-        for c in new_column:
-            cursor.execute("alter table %(table)s add column %(new_column)s \
-                           'float'" % {'table': self.table, 'new_column': c})
-            conn.commit()
-        cursor.close()
