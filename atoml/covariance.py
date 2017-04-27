f7d31f6784f130905438c8c5aa0588e95d5d601e
mhangaard <mhangaard@gmail.com>
2017-04-27 16:28:07 -0700

    add general covariance function and kernels

--- atoml/covariance.py
+++ atoml/covariance.py
@@ -6,6 +6,53 @@ Created on Wed Apr 26 17:27:47 2017
 
 import numpy as np
 from .kernels import kernel, kernel_combine, dkernel_dwidth
+from atoml import kernels as ak
+
+
+def general_covariance(train_matrix, kernel_dict={}, regularization=None):
+    """ Returns the covariance matrix of training dataset.
+
+        Parameters
+        ----------
+        train_matrix : list
+            A list of the training fingerprint vectors.
+    """
+    # Loop over kernels in kernel_dict
+    print(kernel_dict)
+    cov = np.zeros(len(train_matrix),)
+    for key in kernel_dict:
+        # Get the type
+        ktype = str(kernel_dict[key]['ktype'])
+        print(ktype)
+        
+        # Store hyperparameters in theta
+        try:
+            # Polynomials have pairs of hyperparamters kfree, kdegree
+            if ktype == 'polynomial':
+                kfree = kernel_dict[key]['kfree']
+                kdegree = kernel_dict[key]['kdegree']
+                zipped_theta = zip(kfree,kdegree)
+                theta = [hp for k in zipped_theta for hp in k]
+            else:
+                theta = kernel_dict[key]['theta']
+        except KeyError:
+            theta = None
+        
+        # Select a subset of features for the kernel
+        if 'features' in key:
+            train_fp = train_matrix[:, kernel_dict[key]['features']]
+                
+        # Get the covariance matrix
+        if ('operation' in kernel_dict[key] and 
+            kernel_dict[key]['operation'] == 'multiplication'):
+            cov = cov * eval(
+            'ak.'+str(ktype)+'_kernel(train_fp,theta=theta)')
+        else:
+            cov = cov + eval(
+            'ak.'+str(ktype)+'_kernel(train_fp,theta=theta)')
+    if regularization is not None:
+        cov = cov + regularization * np.identity(len(cov))
+    return cov
 
 def get_covariance(train_matrix, ktype='gaussian', 
                    kwidth=None, 
--- atoml/kernels.py
+++ atoml/kernels.py
@@ -8,6 +8,63 @@ Contains kernel functions and gradients of kernels.
 import numpy as np
 from scipy.spatial import distance
 
+def gaussian_kernel(m1, m2=None, theta=None):
+    kwidth = theta
+    if m2 is None:
+        k = distance.pdist(m1 / kwidth, metric='sqeuclidean')
+        k = distance.squareform(np.exp(-.5 * k))
+        np.fill_diagonal(k, 1)
+    else:
+        k = distance.cdist(m1 / kwidth, m2 / kwidth,
+                           metric='sqeuclidean')
+    return np.exp(-.5 * k)
+
+def d_gaussian_kernel(m1, m2=None, theta=None):
+    kwidth = theta
+    n = len(fpm_j)
+    gram = np.zeros([n, n])
+    # Construct Gram matrix.
+    for i, x1 in enumerate(fpm_j):
+        for j, x2 in enumerate(fpm_j):
+            if j >= i:
+                break
+            d_ij = abs(x1-x2)
+            gram[i, j] = d_ij
+            gram[j, i] = d_ij
+    # Insert gram matrix in differentiated kernel.
+    dkdw_j = np.exp(-.5 * gram**2 / (width_j**2)) * (gram**2 /
+                                                     (width_j**3))
+    return dkdw_j
+
+def linear_kernel(m1, m2=None, theta=None):
+    if m2 is None:
+        m2 = m1
+    return np.dot(m1, np.transpose(m2))
+
+def d_linear_kernel(m1, m2=None, theta=None):
+    return np.dot(m1, np.transpose(m2))
+
+def polynomial_kernel(m1, m2=None, theta=None):
+    kfree = theta[::2]
+    kdegree = theta[1::2]
+    if m2 is None:
+        m2 = m1
+    return(np.dot(m1, np.transpose(m2)) + kfree) ** kdegree
+
+def d_polynomial_kernel(m1, m2=None, theta=None):
+    raise NotImplementedError('To Do')
+
+def laplacian_kernel(m1, m2, theta=None):
+    if m2 is None:
+        kwidth = theta
+        k = distance.pdist(m1 / kwidth, metric='cityblock')
+        k = distance.squareform(np.exp(-k))
+        np.fill_diagonal(k, 1)
+    else:
+        k = distance.cdist(m1 / kwidth, m2 / kwidth,
+                               metric='cityblock')
+    return np.exp(-k)
+
 def kernel(ktype, m1, m2=None, kwidth=None, kfree=None, kdegree=None):
     """ Kernel functions taking n x d feature matrix.
 
