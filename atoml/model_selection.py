fix gradient based optimizer for ktype gaussian

atoml/model_selection.py
@@ -11,7 +11,7 @@ import numpy as np
 from scipy.linalg import cholesky, cho_solve
 from numpy.core.umath_tests import inner1d
 from .covariance import get_covariance, gramian
-#from .kernels import dkernel_dwidth
+from .kernels import dkernel_dwidth
 
 def log_marginal_likelihood(theta, train_fp, y, ktype):
     """ Return the log marginal likelyhood.
@@ -31,8 +31,7 @@ def log_marginal_likelihood(theta, train_fp, y, ktype):
     p = (datafit + complexity + normalization).sum()
     return -p
 
-def gradient_log_p(theta, train_fp, y, ktype='gaussian', width_combine=None, 
-                       combine_kernels=None, kernel_list=None):
+def gradient_log_p(theta, train_fp, y, ktype='gaussian'):
     """ Return the gradient of the log marginal likelyhood.
         (Equation 5.9 in C. E. Rasmussen and C. K. I. Williams, 2006)
 
@@ -43,15 +42,11 @@ def gradient_log_p(theta, train_fp, y, ktype='gaussian', width_combine=None,
             noise: float
             y: vector of length n
     """
-    if combine_kernels is not None:
-        raise NotImplementedError
     if ktype is not 'gaussian':
         raise NotImplementedError
     kwidth = theta[:-1]
-    regularization = theta[-1]
-    K = get_covariance(train_fp, kwidth=kwidth, width_combine=None, 
-                       combine_kernels=None, kernel_list=None, 
-                       regularization=regularization)
+    kdict = {'k1': {'type': ktype, 'width': kwidth}}
+    K = gramian(train_fp, kdict, regularization=theta[-1])
     m = len(kwidth)
     n = len(y)
     y = y.reshape([n, 1])

atoml/predict.py
@@ -7,7 +7,7 @@ from __future__ import division
 import numpy as np
 from scipy.optimize import minimize
 from collections import defaultdict
-from .model_selection import log_marginal_likelihood
+from .model_selection import log_marginal_likelihood, gradient_log_p
 from .output import write_predict
 from .covariance import gramian, get_covariance
 
@@ -104,10 +104,10 @@ class GaussianProcess(object):
             # Set bounds for hyperparameters
             bounds = ((1E-6,1e3),)*(len(theta))
             # Optimize
+            #self.theta_opt = minimize(log_marginal_likelihood, theta, 
+            #                     args=args, bounds=bounds)
             self.theta_opt = minimize(log_marginal_likelihood, theta, 
-                                 args=args, bounds=bounds)
-            #theta_opt = minimize(log_marginal_likelihood, theta, 
-            #                     args=a, bounds=b, jac=gradient_log_p)
+                                 args=args, bounds=bounds, jac=gradient_log_p)
             # Update kernel_dict and regularization
             self.kernel_dict[kernelkey]['width'] = self.theta_opt['x'][:-1]
             self.regularization = self.theta_opt['x'][-1]