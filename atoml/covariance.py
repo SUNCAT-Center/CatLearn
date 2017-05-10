Fix covariance.py pep8 complaints.

atoml/covariance.py
@@ -7,7 +7,8 @@ Created on Wed Apr 26 17:27:47 2017
 import numpy as np
 from atoml import kernels as ak
 
-def get_covariance(train_matrix, test_matrix=None, kernel_dict={},
+
+def get_covariance(kernel_dict, train_matrix, test_matrix=None,
                    regularization=None):
     """ Returns the covariance matrix of training dataset.
 
@@ -15,12 +16,12 @@ def get_covariance(train_matrix, test_matrix=None, kernel_dict={},
         ----------
         train_matrix : list
             A list of the training fingerprint vectors.
-        
+
         test_matrix : list
             A list of the test fingerprint vectors.
-            
+
         kernel_dict : dict of dicts
-        
+
         regularization : None or float
     """
     N_train, N_D = np.shape(train_matrix)
@@ -30,11 +31,11 @@ def get_covariance(train_matrix, test_matrix=None, kernel_dict={},
     else:
         N_test = N_train
     # Initialize covariance matrix
-    cov = np.zeros([N_train,N_test])
+    cov = np.zeros([N_train, N_test])
     # Loop over kernels in kernel_dict
     for key in kernel_dict:
         ktype = kernel_dict[key]['type']
-        
+
         # Select a subset of features for the kernel
         if 'features' in kernel_dict[key]:
             train_fp = train_matrix[:, kernel_dict[key]['features']]
@@ -47,16 +48,15 @@ def get_covariance(train_matrix, test_matrix=None, kernel_dict={},
             train_fp = train_matrix
             test_fp = test_matrix
             theta = ak.kdict2list(kernel_dict[key], N_D)
-        
-        # Get the covariance matrix
-        if ('operation' in kernel_dict[key] and 
-            kernel_dict[key]['operation'] == 'multiplication'):
-            cov = cov * eval(
-            'ak.'+str(ktype)+'_kernel(train_fp,test_fp,theta=theta)')
-        else:
-            cov = cov + eval(
-            'ak.'+str(ktype)+'_kernel(train_fp,test_fp,theta=theta)')
-    if regularization is not None:
-        cov = cov + regularization * np.identity(len(cov))
-    return cov
 
+        # Get the covariance matrix
+        if 'operation' in kernel_dict[key] and \
+           kernel_dict[key]['operation'] == 'multiplication':
+            cov *= eval('ak.'+str(ktype) +
+                        '_kernel(train_fp,test_fp,theta=theta)')
+        else:
+            cov += eval('ak.' + str(ktype) +
+                        '_kernel(train_fp,test_fp,theta=theta)')
+    if regularization is not None:
+        cov += regularization * np.identity(len(cov))
+    return cov