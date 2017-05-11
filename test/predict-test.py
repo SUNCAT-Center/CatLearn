602fa416a1d71ba23fce382320358a4a61a90aef
Paul C. Jennings <jennings.p.c@gmail.com>
2017-05-10 21:36:26 -0700

    Update prediction test.

--- test/predict-test.py
+++ test/predict-test.py
@@ -1,8 +1,6 @@
 """ Script to test the prediction functions. """
 from __future__ import print_function
 
-import numpy as np
-
 from ase.ga.data import DataConnection
 from atoml.data_setup import get_unique, get_train
 from atoml.fingerprint_setup import normalize, return_fpv
@@ -28,63 +26,46 @@ test_fp = return_fpv(testset['candidates'], [fpv.nearestneighbour_fpv])
 train_fp = return_fpv(trainset['candidates'], [fpv.nearestneighbour_fpv])
 nfp = normalize(train=train_fp, test=test_fp)
 
-# Set up the prediction routine.
+# Test prediction routine with linear kernel.
 kdict = {'k1': {'type': 'linear'}}
-krr = GaussianProcess(kernel_dict=kdict)
-#krr = FitnessPrediction(ktype='linear')
-#cvm = krr.get_covariance(train_matrix=nfp['train'])
-#cinv = np.linalg.inv(cvm)
-#assert np.shape(cinv) == (50, 50)
-pred = krr.get_predictions(train_fp=nfp['train'],
-                           test_fp=nfp['test'],
-                           cinv=None,
-                           train_target=trainset['target'],
-                           test_target=testset['target'],
-                           get_validation_error=True,
-                           get_training_error=True,
-                           optimize_hyperparameters=False)
+gp = GaussianProcess(kernel_dict=kdict)
+pred = gp.get_predictions(train_fp=nfp['train'],
+                          test_fp=nfp['test'],
+                          cinv=None,
+                          train_target=trainset['target'],
+                          test_target=testset['target'],
+                          get_validation_error=True,
+                          get_training_error=True,
+                          optimize_hyperparameters=False)
 assert len(pred['prediction']) == 10
 print('linear prediction:', pred['validation_rmse']['average'])
 
-# Set up the prediction routine.
+# Test prediction routine with polynomial kernel.
 kdict = {'k1': {'type': 'polynomial', 'kfree': 0., 'kdegree': 2.}}
-krr = GaussianProcess(kernel_dict=kdict)
-#krr = FitnessPrediction(ktype='polynomial',
-#                        kfree=0.,
-#                        kdegree=2.)
-#cvm = krr.get_covariance(train_matrix=nfp['train'])
-#cinv = np.linalg.inv(cvm)
-#assert np.shape(cinv) == (50, 50)
-pred = krr.get_predictions(train_fp=nfp['train'],
-                           test_fp=nfp['test'],
-                           cinv=None,
-                           train_target=trainset['target'],
-                           test_target=testset['target'],
-                           get_validation_error=True,
-                           get_training_error=True,
-                           optimize_hyperparameters=False)
+gp = GaussianProcess(kernel_dict=kdict)
+pred = gp.get_predictions(train_fp=nfp['train'],
+                          test_fp=nfp['test'],
+                          cinv=None,
+                          train_target=trainset['target'],
+                          test_target=testset['target'],
+                          get_validation_error=True,
+                          get_training_error=True,
+                          optimize_hyperparameters=False)
 assert len(pred['prediction']) == 10
 print('polynomial prediction:', pred['validation_rmse']['average'])
 
-# Set up the prediction routine.
+# Test prediction routine with gaussian kernel.
 kdict = {'k1': {'type': 'gaussian', 'width': 0.5}}
-krr = GaussianProcess(kernel_dict=kdict,
-                        regularization=0.001)
-#krr = FitnessPrediction(ktype='gaussian',
-#                        kwidth=0.5,
-#                        regularization=0.001)
-#cvm = krr.get_covariance(train_matrix=nfp['train'])
-#cinv = np.linalg.inv(cvm)
-#assert np.shape(cinv) == (50, 50)
-pred = krr.get_predictions(train_fp=nfp['train'],
-                           test_fp=nfp['test'],
-                           cinv=None,
-                           train_target=trainset['target'],
-                           test_target=testset['target'],
-                           get_validation_error=True,
-                           get_training_error=True,
-                           uncertainty=True,
-                           cost='squared')
+gp = GaussianProcess(kernel_dict=kdict, regularization=0.001)
+pred = gp.get_predictions(train_fp=nfp['train'],
+                          test_fp=nfp['test'],
+                          cinv=None,
+                          train_target=trainset['target'],
+                          test_target=testset['target'],
+                          get_validation_error=True,
+                          get_training_error=True,
+                          uncertainty=True,
+                          cost='squared')
 assert len(pred['prediction']) == 10
 print('gaussian prediction:', pred['validation_rmse']['average'])
 for i, j, k, in zip(pred['prediction'],
@@ -92,93 +73,68 @@ for i, j, k, in zip(pred['prediction'],
                     pred['validation_rmse']['all']):
     print(i, j, k)
 
-pred = krr.get_predictions(train_fp=nfp['train'],
-                           test_fp=nfp['test'],
-                           cinv=None,
-                           train_target=trainset['target'],
-                           test_target=testset['target'],
-                           get_validation_error=True,
-                           get_training_error=True,
-                           cost='absolute')
+pred = gp.get_predictions(train_fp=nfp['train'],
+                          test_fp=nfp['test'],
+                          cinv=None,
+                          train_target=trainset['target'],
+                          test_target=testset['target'],
+                          get_validation_error=True,
+                          get_training_error=True,
+                          cost='absolute')
 print('gaussian prediction (abs):', pred['validation_rmse']['average'])
 
-pred = krr.get_predictions(train_fp=nfp['train'],
-                           test_fp=nfp['test'],
-                           cinv=None,
-                           train_target=trainset['target'],
-                           test_target=testset['target'],
-                           get_validation_error=True,
-                           get_training_error=True,
-                           cost='insensitive',
-                           epsilon=0.1)
+pred = gp.get_predictions(train_fp=nfp['train'],
+                          test_fp=nfp['test'],
+                          cinv=None,
+                          train_target=trainset['target'],
+                          test_target=testset['target'],
+                          get_validation_error=True,
+                          get_training_error=True,
+                          cost='insensitive',
+                          epsilon=0.1)
 print('gaussian prediction (ins):', pred['validation_rmse']['average'])
 
-# Set up the prediction routine.
+# Test prediction routine with laplacian kernel.
 kdict = {'k1': {'type': 'laplacian', 'width': 0.5}}
-krr = GaussianProcess(kernel_dict=kdict,
-                        regularization=0.001)
-#krr = FitnessPrediction(ktype='laplacian',
-#                        kwidth=0.5,
-#                        regularization=0.001)
-#cvm = krr.get_covariance(train_matrix=nfp['train'])
-#cinv = np.linalg.inv(cvm)
-#assert np.shape(cinv) == (50, 50)
-pred = krr.get_predictions(train_fp=nfp['train'],
-                           test_fp=nfp['test'],
-                           cinv=None,
-                           train_target=trainset['target'],
-                           test_target=testset['target'],
-                           get_validation_error=True,
-                           get_training_error=True,
-                           optimize_hyperparameters=True)
+gp = GaussianProcess(kernel_dict=kdict, regularization=0.001)
+pred = gp.get_predictions(train_fp=nfp['train'],
+                          test_fp=nfp['test'],
+                          cinv=None,
+                          train_target=trainset['target'],
+                          test_target=testset['target'],
+                          get_validation_error=True,
+                          get_training_error=True,
+                          optimize_hyperparameters=True)
 assert len(pred['prediction']) == 10
 print('laplacian prediction:', pred['validation_rmse']['average'])
 
-# Set up the prediction routine.
+# Test prediction routine with addative linear and gaussian kernel.
 kdict = {'k1': {'type': 'linear', 'features': [0, 1]},
-                'k2': {'type': 'gaussian', 'features': [2, 3], 'width': 0.5}
-                }
-krr = GaussianProcess(kernel_dict=kdict,
-                        regularization=0.001)
-#krr = FitnessPrediction(combine_kernels='addition',
-#                        kernel_list={'linear': [0, 1], 'gaussian': [2, 3]},
-#                        kwidth=0.5,
-#                        regularization=0.001)
-#cvm = krr.get_covariance(train_matrix=nfp['train'])
-#cinv = np.linalg.inv(cvm)
-#assert np.shape(cinv) == (50, 50)
-pred = krr.get_predictions(train_fp=nfp['train'],
-                           test_fp=nfp['test'],
-                           cinv=None,
-                           train_target=trainset['target'],
-                           test_target=testset['target'],
-                           get_validation_error=True,
-                           get_training_error=True,
-                           optimize_hyperparameters=True)
+         'k2': {'type': 'gaussian', 'features': [2, 3], 'width': 0.5}}
+gp = GaussianProcess(kernel_dict=kdict, regularization=0.001)
+pred = gp.get_predictions(train_fp=nfp['train'],
+                          test_fp=nfp['test'],
+                          cinv=None,
+                          train_target=trainset['target'],
+                          test_target=testset['target'],
+                          get_validation_error=True,
+                          get_training_error=True,
+                          optimize_hyperparameters=True)
 assert len(pred['prediction']) == 10
 print('addition prediction:', pred['validation_rmse']['average'])
 
-# Set up the prediction routine.
+# Test prediction routine with multiplication of linear and gaussian kernel.
 kdict = {'k1': {'type': 'linear', 'features': [0, 1]},
-                'k2': {'type': 'gaussian', 'features': [2, 3], 'width': 0.5,
-                       'operation': 'multiplication'}
-                }
-krr = GaussianProcess(kernel_dict=kdict,
-                        regularization=0.001)
-#krr = FitnessPrediction(combine_kernels='multiplication',
-#                        kernel_list={'linear': [0, 1], 'gaussian': [2, 3]},
-#                        kwidth=0.5,
-#                        regularization=0.001)
-#cvm = krr.get_covariance(train_matrix=nfp['train'])
-#cinv = np.linalg.inv(cvm)
-#assert np.shape(cinv) == (50, 50)
-pred = krr.get_predictions(train_fp=nfp['train'],
-                           test_fp=nfp['test'],
-                           cinv=None,
-                           train_target=trainset['target'],
-                           test_target=testset['target'],
-                           get_validation_error=True,
-                           get_training_error=True,
-                           optimize_hyperparameters=True)
+         'k2': {'type': 'gaussian', 'features': [2, 3], 'width': 0.5,
+                'operation': 'multiplication'}}
+gp = GaussianProcess(kernel_dict=kdict, regularization=0.001)
+pred = gp.get_predictions(train_fp=nfp['train'],
+                          test_fp=nfp['test'],
+                          cinv=None,
+                          train_target=trainset['target'],
+                          test_target=testset['target'],
+                          get_validation_error=True,
+                          get_training_error=True,
+                          optimize_hyperparameters=True)
 assert len(pred['prediction']) == 10
 print('multiplication prediction:', pred['validation_rmse']['average'])
