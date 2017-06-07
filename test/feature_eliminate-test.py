9544d2abba0036616f1cd8bf764786663aa05646
Paul C. Jennings <jennings.p.c@gmail.com>
2017-06-06 22:17:05 -0700

    Small update to tests.

--- test/feature_eliminate-test.py
+++ test/feature_eliminate-test.py
@@ -1,5 +1,6 @@
 """Script to test the fingerprint generation functions."""
 from __future__ import print_function
+from __future__ import absolute_import
 
 import numpy as np
 
--- test/fingerprint-test.py
+++ test/fingerprint-test.py
@@ -1,5 +1,6 @@
-""" Script to test the fingerprint generation functions. """
+"""Script to test the fingerprint generation functions."""
 from __future__ import print_function
+from __future__ import absolute_import
 
 import time
 
