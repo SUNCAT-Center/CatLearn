3ac5990cc669a2b4a512e33c1db22d11896c117b
Paul C. Jennings <jennings.p.c@gmail.com>
2017-06-29 15:54:30 -0700

    General cleaning of the feature_dist file.

--- example/data_analysis/feature_dist.py
+++ example/data_analysis/feature_dist.py
@@ -1,4 +1,4 @@
-"""Plot correlation between features."""
+"""Plot feature distribution sorted by correlation with target."""
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -7,10 +7,11 @@ from scipy.stats import pearsonr, spearmanr, kendalltau
 
 from atoml.cross_validation import HierarchyValidation
 from atoml.feature_preprocess import standardize
+from atoml.utilities import clean_variance
 
-flen = 100
-uncorr = False
-correlation = 'pearson'
+flen = 100  # Number of features to check.
+cut = True  # True for best correlated, False for least.
+correlation = 'pearson'  # Type of correlation analysis.
 
 # Define the hierarchey cv class method.
 hv = HierarchyValidation(db_name='../../data/train_db.sqlite',
@@ -24,44 +25,37 @@ ind = hv.load_split()
 # Split out the various data.
 train_data = np.array(hv._compile_split(ind['1_1'])[:, 1:-1], np.float64)
 train_target = np.array(hv._compile_split(ind['1_1'])[:, -1:], np.float64)
-test_data = np.array(hv._compile_split(ind['1_2'])[:, 1:-1], np.float64)
-test_target = np.array(hv._compile_split(ind['1_2'])[:, -1:], np.float64)
+train_data = clean_variance(train_data)['train']
 
 # Scale and shape the data.
-std = standardize(train_matrix=train_data, test_matrix=test_data)
-train_data, test_data = std['train'], std['test']
+train_data = standardize(train_matrix=train_data)['train']
+d, f = np.shape(train_data)
 train_target = train_target.reshape(len(train_target), )
-test_target = test_target.reshape(len(test_target), )
+traint = np.transpose(train_data)
+index = list(range(f))
 
 corr = []
-for c in train_data.T:
+for c in traint:
     if correlation is 'pearson':
         corr.append(pearsonr(c, train_target)[0])
     elif correlation is 'spearman':
         corr.append(spearmanr(c, train_target)[0])
     elif correlation is 'kendall':
         corr.append(kendalltau(c, train_target)[0])
-ind = list(range(len(corr)))
 
-sort_list = [list(i) for i in zip(*sorted(zip(ind, corr), key=lambda x: x[1],
-                                          reverse=True))]
+sort_list = [list(i) for i in zip(*sorted(zip(index, np.abs(corr)),
+                                          key=lambda x: x[1], reverse=cut))]
 
-if uncorr:
-    delf = sort_list[0][:len(ind)-flen]
-else:
-    delf = sort_list[0][flen:]
-cut_ind = np.delete(ind, delf)
-print(cut_ind)
+delf = sort_list[0][flen:]
+cut_index, cut_corr = np.delete(index, delf), np.delete(corr, delf)
+print(cut_index, cut_corr)
 train_data = np.delete(train_data, delf, axis=1)
-print(np.shape(train_data))
 
+data = {}
 traint = np.transpose(train_data)
-ind = list(range(len(traint)))
-
-d = {}
-for i, j in zip(traint, ind):
-    d[j] = i
-df = pd.DataFrame(d)
+for i, j in zip(traint, index):
+    data[j] = i
+df = pd.DataFrame(data)
 
 ax = sns.violinplot(data=df, inner=None)
 plt.show()
