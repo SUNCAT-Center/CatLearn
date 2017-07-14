bdbe467d9323222fb1f759134530ec2a72c4a06a
Paul C. Jennings <jennings.p.c@gmail.com>
2017-07-14 12:05:32 -0700

    Clean up elimination test and rename.

--- test/feature_optimization-test.py
+++ test/feature_optimization-test.py
@@ -1,4 +1,4 @@
-"""Script to test the fingerprint generation functions."""
+"""Script to test feature space optimization functions."""
 from __future__ import print_function
 from __future__ import absolute_import
 
@@ -7,17 +7,19 @@ import numpy as np
 from atoml.database_functions import DescriptorDatabase
 from atoml.feature_elimination import FeatureScreening
 
+# Attach the database.
 dd = DescriptorDatabase(db_name='fpv_store.sqlite', table='FingerVector')
+
+# Pull the features and targets from the database.
 names = dd.get_column_names()
 features, targets = names[1:-1], names[-1:]
-
 feature_data = dd.query_db(names=features)
 target_data = np.reshape(dd.query_db(names=targets),
                          (np.shape(feature_data)[0], ))
 
+# Split the data into so test and training sets.
 train_features, train_targets = feature_data[:35, :], target_data[:35]
 test_features, test_targets = feature_data[35:, :], target_data[35:]
-
 d, f = np.shape(train_features)
 
 # Get descriptor correlation
@@ -31,7 +33,7 @@ for c in corr:
     assert np.shape(features[0])[1] == d and np.shape(features[1])[1] == d
 
     screen = FeatureScreening(correlation=c, iterative=True,
-                              regression='ridge')
+                              regression='lasso')
     features = screen.eliminate_features(target=train_targets,
                                          train_features=train_features,
                                          test_features=test_features,
