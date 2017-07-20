6f1ee46775f111177354d3768d22ee40b67873d2
Jacob Boes <jacobboes@gmail.com>
2017-07-20 15:02:18 -0700

    JSON version of the general fingerprint parameter collection code

--- atoml/general_fingerprint.py
+++ atoml/general_fingerprint.py
@@ -1,14 +1,18 @@
-from ase.db import connect
+import json
+
+# Load the Mendeleev parameter data into memory
+with open('../data/proxy-mendeleev.json') as f:
+    data = json.load(f)
 
 
-def get_mendeleev_params(d, index=None, extra_params=[]):
+def get_mendeleev_params(atomic_number, index=None, extra_params=[]):
     """ Return a list of generic parameters about
     an indexed atom from an ase-database entry.
 
     Parameters
     ----------
-    d : ase-database instance, str, int
-        An ase-database entry, atomic symbol, or atomic number.
+    atomic_number : int
+        An atomic number.
     index : int
         The atom index whose parameters to return.
     extra_params: list of str
@@ -26,35 +30,26 @@ def get_mendeleev_params(d, index=None, extra_params=[]):
 
     # Parameters which typically do not contain None
     params = [
-	'atomic_number',
-	'atomic_volume',
-	'boiling_point',
-	'density',
-	'dipole_polarizability',
-	'electron_affinity',
-	'group_id',
-	'lattice_constant',
-	'melting_point',
-	'period',
-	'vdw_radius',
-	'covalent_radius_cordero',
-	'en_allen',
-	'atomic_weight']
-
-    # Pull parameters from a proxy database for
-    # improved performance.
-    db = connect('../data/proxy-mendeleev.db')
+        'atomic_number',
+        'atomic_volume',
+        'boiling_point',
+        'density',
+        'dipole_polarizability',
+        'electron_affinity',
+        'group_id',
+        'lattice_constant',
+        'melting_point',
+        'period',
+        'vdw_radius',
+        'covalent_radius_cordero',
+        'en_allen',
+        'atomic_weight']
 
     if extra_params:
 	params += extra_params
 
-    if isinstance(d, str) or isinstance(d, int):
-        sym = d
-    else:
-        sym = d.symbols[index]
-
     var = []
     for _ in params:
-	var += [db.get(sym).data.get(_)]
+	var += [data[str(atomic_number)].get(_)]
 
     return var
