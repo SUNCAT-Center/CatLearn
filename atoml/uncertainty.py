9b17c6c65053b2630e9e0700463ea47a247bdac3
Paul C. Jennings <jennings.p.c@gmail.com>
2017-08-18 10:03:05 -0700

    Add a fitness classification function to uncertainty.

--- atoml/uncertainty.py
+++ atoml/uncertainty.py
@@ -24,8 +24,10 @@ def get_uncertainty(kernel_dict, test_fp, reg, ktb, cinv):
     cinv : array
         Covariance matrix for training dataset.
     """
+    # Generate the test covariance matrix.
     kxx = get_covariance(kernel_dict=kernel_dict,
                          matrix1=test_fp)
+    # Calculate the prediction variance for test data.
     u = [(reg + kxx[kt][kt] - np.dot(np.dot(ktb[kt], cinv),
                                      np.transpose(ktb[kt]))) **
          0.5 for kt in range(len(ktb))]
@@ -33,7 +35,33 @@ def get_uncertainty(kernel_dict, test_fp, reg, ktb, cinv):
     return u
 
 
-def cdf_fit(x, m, v):
+def classify_uncertainty(test_dict, train_dict):
+    """Function to classify how good something is based of CDF.
+
+    Parameters
+    ----------
+    test_dict : dict
+        Information from test data. For each system, there should be a
+        class identifier, the prediction and uncertainty.
+    train_dict : dict
+        Information from training data. For each known class, there should
+        be known property.
+    """
+    for i in test_dict:
+        c = i['class']
+        if c not in train_dict['class']:
+            cdf = float('inf')
+        else:
+            x = train_dict[c]
+            m = test_dict[i]['pred']
+            v = test_dict[i]['uncertainty']
+            cdf = _cdf_fit(x=x, m=m, v=v)
+    test_dict[i]['fit'] = cdf
+
+    return test_dict
+
+
+def _cdf_fit(x, m, v):
     """Calculate the cumulative distribution function.
 
     Parameters
