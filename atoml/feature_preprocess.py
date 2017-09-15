4e15f7d40d3344f876a935d2c140a98943bc86f6
Paul C. Jennings <jennings.p.c@gmail.com>
2017-09-15 09:37:13 -0700

    Remove double definition of function.

--- atoml/feature_preprocess.py
+++ atoml/feature_preprocess.py
@@ -4,29 +4,6 @@ from scipy import cluster
 from collections import defaultdict
 
 
-def matrix_split(X, nsplit, fix_size=None):
-    """Routine to split feature matrix and return sublists.
-
-    Parameters
-    ----------
-    nsplit : int
-        The number of bins that data should be devided into.
-    fix_size : int
-        Define a fixed sample size, e.g. nsplit=5 fix_size=100, generates
-        5 x 100 data split. Default is None, all available data is divided
-        nsplit times.
-    """
-    np.random.shuffle(X)  # Shuffle ordering of the array along 0 axis.
-    if fix_size is not None:
-        msg = 'Cannot divide dataset in this way, number of candidates is '
-        msg += 'too small'
-        assert np.shape(X)[0] >= nsplit * fix_size, msg
-
-        X = X[:nsplit * fix_size, :]
-
-    return np.array_split(X, nsplit)
-
-
 def standardize(train_matrix, test_matrix=None, mean=None, std=None,
                 local=True):
     """Standardize each feature relative to the mean and standard deviation.
