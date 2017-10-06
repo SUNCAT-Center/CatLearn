610c20f188528cb2a59ece6289c44c26decf1e56
Paul C. Jennings <jennings.p.c@gmail.com>
2017-10-06 10:21:33 -0700

    Update GP sensitivity function.

--- atoml/regression/gpfunctions/sensitivity.py
+++ atoml/regression/gpfunctions/sensitivity.py
@@ -4,8 +4,8 @@ from __future__ import division
 
 import numpy as np
 
-from .predict import GaussianProcess
-from .covariance import get_covariance
+from atoml.regression import GaussianProcess
+from atoml.regression.gpfunctions.covariance import get_covariance
 
 
 class SensitivityAnalysis(object):
@@ -169,9 +169,13 @@ class SensitivityAnalysis(object):
         if self.test_targets is not None:
             ve = True
         # Test data.
-        pred = gp.get_predictions(test_fp=self.test_matrix,
-                                  test_target=self.test_targets,
-                                  get_validation_error=ve,
-                                  get_training_error=True)
+        pred = gp.predict(test_fp=self.test_matrix,
+                          test_target=self.test_targets,
+                          get_validation_error=ve,
+                          get_training_error=True)
+
+        print('{1} feature prediction ({0:.3f}):'.format(
+            pred['validation_error']['rmse_average'],
+            np.shape(self.test_matrix)[1]))
 
         return pred
