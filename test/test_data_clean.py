7b7242675e7b72f7802666cd238f964581b22008
Paul C. Jennings <jennings.p.c@gmail.com>
2017-11-06 10:44:27 -0800

    Separate out data cleaning utilities and remove dependence on atoms object from outliers.

---
+++ test/test_data_clean.py
@@ -0,0 +1,16 @@
+"""Simple tests for data cleaning."""
+import numpy as np
+
+from atoml.utilities.clean_data import remove_outliers
+
+f = np.arange(200).reshape(50, 4)
+
+t = np.random.random_sample((50,))
+t[2] += 200000.
+t[0] -= 200000.
+
+d = remove_outliers(features=f, targets=t, constraint=None)
+
+assert np.shape(d['features']) != np.shape(f)
+assert np.shape(d['targets']) != np.shape(t)
+assert np.shape(d['features'])[0] == np.shape(d['targets'])[0]
