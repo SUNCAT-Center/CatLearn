c9a5c8d6e12a6e5ba87fa7ecc3fa3a4eb2505e8e
Jose A Garrido Torres <jagt@stanford.edu>
2017-11-14 11:48:14 -0800

    Added standardization of the features (train, test) and targets (target and gradients).

--- tutorials/5_toy_model_gradients/toy_model.py
+++ tutorials/5_toy_model_gradients/toy_model.py
@@ -13,8 +13,19 @@ from atoml.preprocess.scale_target import target_standardize
 from atoml.regression import GaussianProcess
 from atoml.utilities.cost_function import get_error
 
+# Standardization
+def Standardize(inputarray):
+    mean = np.mean(inputarray)
+    std = np.std(inputarray)
+    standardizedarray = (inputarray-mean)/std
+    return standardizedarray, mean, std
+
+StandardizeFeatures = True
+StandardizeTargets = True
+
 # First derivative observations can be included.
 eval_gradients = True
+
 
 # A known underlying function in one dimension and it's first derivative.
 def afunc(x):
@@ -29,7 +40,7 @@ def afunc(x):
 
 # A number of training points in x.
 train = np.array([[1.0, 2.0, 4.0, 4.8, 5.0, 5.5, 5.8, 6.0]])
-
+train = np.reshape(train, (np.shape(train)[1], 1))
 
 # Each element in the list train can be referred to as a fingerprint.
 
@@ -37,47 +48,36 @@ train = np.array([[1.0, 2.0, 4.0, 4.8, 5.0, 5.5, 5.8, 6.0]])
 target = np.array(afunc(train)[0])
 
 # Generate test datapoints x.
-test_points = 100
+test_points = 200
 test = np.linspace(0.1,6.0,test_points)
-
-# Store standard deviations of the training data and targets.
-# stdx = np.std(train)
-# stdy = np.std(target)
-# tstd = 2.
-
-
-# Train and test data without standardizing.
-
-train = np.reshape(train, (np.shape(train)[1], 1))
 test =np.reshape(test, (test_points, 1))
 
-# # Standardize the training and test data on the same scale.
-# std = standardize(train_matrix=np.reshape(train, (np.shape(train)[1], 1)),
-#                   test_matrix=np.reshape(test, (test_points, 1)))
-# # Standardize the training targets.
-# train_targets = target_standardize(target[0])
-
-# Note that predictions will now be made on the standardized scale.
+# Standardization of the train, test and train_test (target) data.
 
 org_target = target.copy()
 org_train = train.copy()
 org_test = test.copy()
 
+if StandardizeFeatures:
+    train, train_mean, train_std = Standardize(train)
+    test = (test -train_mean)/train_std
+else:
+    train_mean, train_std = 0.0, 1.0
+if StandardizeTargets:
+    target, target_mean, target_std = Standardize(target)
+else:
+    target_mean, target_std = 0.0, 1.0
+
 # Gradients
 
 if eval_gradients == True:
     gradients = []
-    for i in train:
+    for i in org_train:
         gradients.append(afunc(i)[1])
     org_gradients = np.asarray(gradients)
-    y_tilde = []
-    # Build y_tilde as:
-    # y_tilde = y1, y2...yN, delta1, delta2...deltaN
+    gradients = org_gradients/(target_std/train_std)
     y_tilde = np.append(target, gradients)
-    y_tilde = np.reshape(y_tilde,(np.shape(y_tilde)[0],1))
-    target = y_tilde
-    target = np.reshape(target,(np.shape(target)[1],np.shape(target)[0]))
-
+    target = np.reshape(y_tilde,(np.shape(y_tilde)[0],1))
 
 # Model example - Gaussian Process
 
@@ -89,31 +89,41 @@ w1 = 1.0  # Too large widths results in a biased model.
 kdict = {'k1': {'type': 'gaussian', 'width': [w1], 'scaling': 1.0}}
 gp = GaussianProcess(kernel_dict=kdict, regularization=sdt1**2,
                      train_fp=train,
-                     train_target=target[0],
+                     train_target=target,
                      optimize_hyperparameters=True,
                      eval_gradients=eval_gradients)
 print('Optimized kernel:', gp.kernel_dict)
 # Do the optimized predictions.
-optimized = gp.predict(test_fp=test, uncertainty=True)
+pred = gp.predict(test_fp=test, uncertainty=True)
+
 # Scale predictions back to the original scale.
 
-# opt_prediction = np.array(optimized['prediction']) * train_targets['std'] + \
+# opt_prediction = np.array(pred['prediction']) * train_targets['std'] + \
 #     train_targets['mean']
-# opt_uncertainty = np.array(optimized['uncertainty']) * train_targets['std']
-opt_prediction = np.array(optimized['prediction'])
-opt_uncertainty = np.array(optimized['uncertainty'])
+# opt_uncertainty = np.array(pred['uncertainty']) * train_targets['std']
+prediction = np.array(pred['prediction'][:,0])
+uncertainty = np.array(pred['uncertainty'])
+
 
 # Get average errors.
-error = get_error(opt_prediction, afunc(test)[0])
+error = get_error(prediction, afunc(test)[0])
 print('Gaussian linear regression prediction:', error['absolute_average'])
 # Get confidence interval on predictions.
-opt_upper = opt_prediction + opt_uncertainty #* tstd
-opt_lower = opt_prediction - opt_uncertainty #* tstd
+upper = prediction + uncertainty
+lower = prediction - uncertainty
+
+
+# Unstandardization:
+if StandardizeTargets:
+    uncertainty = (uncertainty*target_std) + target_mean
+    prediction = (prediction*target_std) + target_mean
+    upper = (upper*target_std) + target_mean
+    lower = (lower*target_std) + target_mean
 
 # Plotting.
 # Store the known underlying function for plotting.
 
-linex = np.linspace(0.1,6.0,1000)
+linex = np.linspace(0.1,6.0,test_points)
 linex = np.reshape(linex, (1,np.shape(linex)[0]))
 linex = np.sort(linex)
 liney = []
@@ -125,12 +135,13 @@ fig = plt.figure(figsize=(5, 5))
 # Example
 ax = fig.add_subplot(111)
 ax.plot(linex[0], liney[0], '-', lw=1, color='black')
-ax.plot(train, org_target[0], 'o', alpha=0.2, color='black')
-ax.plot(test, opt_prediction, 'g-', lw=1, alpha=0.4)
-ax.fill_between(test[:,0], opt_upper, opt_lower, interpolate=True, \
+ax.plot(org_train, org_target, 'o', alpha=0.2, color='black')
+ax.plot(org_test, prediction, 'g-', lw=1, alpha=0.4)
+ax.fill_between(org_test[:,0], upper, lower,
+interpolate=True, \
 color='green',
                 alpha=0.2)
-plt.title('Optimized GP. \n w: {0:.3f}, r: {1:.3f}'.format(
+plt.title('GP. \n w: {0:.3f}, r: {1:.3f}'.format(
     gp.kernel_dict['k1']['width'][0], np.sqrt(gp.regularization)))
 plt.xlabel('Descriptor')
 plt.ylabel('Response')
@@ -139,8 +150,6 @@ plt.axis('tight')
 # Gradients
 
 if eval_gradients==True:
-    org_target = np.reshape(org_target,(np.shape(org_target)[1], np.shape(
-    org_target)[0]))
     size_bar_gradients = (np.abs(np.max(linex) - np.min(linex))/2.0)/25.0
     def lineary(m,linearx,train,target):
             """Define some linear function."""
