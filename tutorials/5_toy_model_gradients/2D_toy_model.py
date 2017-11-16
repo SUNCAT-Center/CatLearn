baa9f2735969c8949e366063b7545a84229dbdae
Jose A Garrido Torres <jagt@stanford.edu>
2017-11-16 15:33:32 -0800

    2D gradients toy_model added. Only for gradients=false [wip]

--- tutorials/5_toy_model_gradients/2D_toy_model.py
+++ tutorials/5_toy_model_gradients/2D_toy_model.py
@@ -15,34 +15,56 @@ from atoml.utilities.cost_function import get_error
 
 
 # The user can choose whether the features and/or the targets are standardized.
-StandardizeFeatures = False
-StandardizeTargets = False
+StandardizeFeatures = True
+StandardizeTargets = True
 
 # First derivative observations can be included.
-eval_gradients = True
+eval_gradients = False
+train_points = 6 # Number of training points.
+test_points = 25 # Length of the grid test points (nxn).
 
 # A known underlying function in one dimension [y] and first derivative [dy].
-def afunc(x):
-    """ Function [y] and first derivative [dy] """
-    y =  np.sin(x)*np.cos(x)*np.exp(2*x)*x**-2*np.exp(-x)*np.cos(x)*np.sin(x)
-    dy =  (2 * np.exp(x)*np.cos(x)**3* np.sin(x))/x**2-(2*np.exp(x)* np.cos(
-    x)**2* np.sin(x)**2)/x**3+(np.exp(x)*np.cos(x)**2 *np.sin(x)**2)/x**2-(
-    2*np.exp(x)*np.cos(x)*np.sin(x)**3)/x**2
-    return [y,dy]
+def afunc(x,y):
+    """S FUNCTION"""
+    z = -(12.0)*(x**2.0) + (1.0/3.0)*(x**4.0)
+    z = z -(12.0)*(y**2.0) + (1.0/2.0)*(y**4.0)
+    dx = -24.0*x + (4.0/3.0)*x**3.0
+    dy = -24.0*y + 2.0*y**3.0
+    return [z,dx,dy]
 
 # Setting up data.
 
 # A number of training points in x.
 # Each element in the list train can be referred to as a fingerprint.
-train = np.array([[1.0], [2.0], [3.0], [4.0]])
+
+train = []
+trainx = np.linspace(-5.0, 5.0, train_points)
+trainy = np.linspace(-5.0, 5.0, train_points)
+for i in range(len(trainx)):
+    for j in range(len(trainy)):
+        train1 = [trainx[i],trainy[j]]
+        train.append(train1)
+train = np.array(train)
+train = np.append(train,([[0.0,0.0]]),axis=0)
+
 
 # Call the underlying function to produce the target values.
-target = np.array(afunc(train)[0])
+target = []
+for i in train:
+    target.append([afunc(i[0],i[1])[0]])
+target = np.array(target)
+
 
 # Generate test datapoints x.
-test_points = 100
-test = np.linspace(0.1,6.0,test_points)
-test = np.reshape(test, (test_points, 1))
+test = []
+testx = np.linspace(-5.0, 5.0, test_points)
+testy = np.linspace(-5.0, 5.0, test_points)
+for i in range(len(testx)):
+    for j in range(len(testy)):
+        test1 = [testx[i],testy[j]]
+        test.append(test1)
+test = np.array(test)
+
 
 # Make a copy of the original features and targets.
 
@@ -87,18 +109,12 @@ w1 = 1.0  # Too large widths results in a biased model.
 # Set up the prediction routine and optimize hyperparameters.
 kdict = {'k1': {'type': 'gaussian', 'width': [w1], 'scaling': 1.0}}
 
-# kdict = {'k1': {'type': 'gaussian', 'width': [w1], 'scaling': 1.0}, 'k2': {
-# 'type': 'linear', 'scaling': 1.0}}
-
-# kdict = {'k1': {'type': 'gaussian', 'width': [w1], 'scaling': 1.0}, 'k2': {
-# 'type': 'constant','const': 1.0, 'scaling': 1.0}}
-
 gp = GaussianProcess(kernel_dict=kdict, regularization=sdt1**2,
                      train_fp=train,
                      train_target=target,
                      optimize_hyperparameters=True,
                      eval_gradients=eval_gradients, algomin='TNC',
-                     global_opt=False)
+                     global_opt=True)
 print('Optimized kernel:', gp.kernel_dict)
 
 # Do the optimized predictions.
@@ -119,50 +135,51 @@ if StandardizeTargets:
     upper = (upper*target_std) + target_mean
     lower = (lower*target_std) + target_mean
 
-# Get average errors.
-error = get_error(prediction, afunc(test)[0])
-print('Gaussian linear regression prediction:', error['absolute_average'])
 
-# Plotting.
+# Plots.
 
-# Store the known underlying function for plotting.
 
-linex = np.linspace(0.1,6.0,test_points)
-linex = np.reshape(linex, (1,np.shape(linex)[0]))
-linex = np.sort(linex)
-liney = []
-for i in linex:
-    liney.append(afunc(i)[0])
+plt.figure(figsize=(8.0, 5.0))
 
-fig = plt.figure(figsize=(5, 5))
+# Contour plot for real function.
 
-# Example
-ax = fig.add_subplot(111)
-ax.plot(linex[0], liney[0], '-', lw=1, color='black')
-ax.plot(org_train, org_target, 'o', alpha=0.2, color='black')
-ax.plot(org_test, prediction, 'g-', lw=1, alpha=0.4)
-ax.fill_between(org_test[:,0], upper, lower, interpolate=True,
-color='green', alpha=0.2)
-# plt.title('GP. \n w: {0:.3f}, r: {1:.3f}'.format(
-#     gp.kernel_dict['k1']['width'][0], np.sqrt(gp.regularization)))
-plt.xlabel('Descriptor')
-plt.ylabel('Response')
-plt.axis('tight')
+plt.subplot(121)
+x = np.linspace(-5.0, 5.0, test_points)
+y = np.linspace(-5.0, 5.0, test_points)
+X,Y = np.meshgrid(x, y)
+plt.contourf(X, Y, afunc(X, Y)[0], 6, alpha=.70, cmap='PRGn',vmin=np.min(afunc(
+X,Y)[0]),
+vmax=np.max(afunc(X,Y)[0]))
+plt.colorbar(orientation="horizontal", pad=0.1)
+plt.clim(np.min(afunc(
+X,Y)[0]),np.max(afunc(
+X,Y)[0]))
+C = plt.contour(X, Y, afunc(X, Y)[0], 6, colors='black', linewidths=1)
+plt.clabel(C, inline=1, fontsize=9)
+plt.title('Real function',fontsize=10)
 
-# Gradients
+# Contour plot for predicted function.
 
-if eval_gradients==True:
-    size_bar_gradients = (np.abs(np.max(linex) - np.min(linex))/2.0)/25.0
-    def lineary(m,linearx,train,target):
-            """Define some linear function."""
-            lineary = m*(linearx-train)+target
-            return lineary
-    for i in range(0,np.shape(org_gradients)[0]):
-        linearx_i = np.linspace(org_train[i]-size_bar_gradients, org_train[i]+
-        size_bar_gradients,num=10)
-        lineary_i = lineary(org_gradients[i],linearx_i,org_train[i],
-        org_target[i])
-        ax.plot(linearx_i, lineary_i, '-', lw=3, alpha=0.5, color='black')
+plt.subplot(122)
+
+x = []
+for i in range(len(test)):
+    t = org_test[i][0]
+    t = x.append(t)
+y = []
+for i in range(len(test)):
+    t = org_test[i][1]
+    t = y.append(t)
+
+
+zi = plt.mlab.griddata(x, y, prediction, testx, testy, interp='linear')
+
+plt.contourf(testx, testy, zi, 6, alpha=.70, cmap='PRGn',vmin=np.min(afunc(
+X,Y)[0]),
+vmax=np.max(afunc(X,Y)[0]))
+plt.colorbar(orientation="horizontal", pad=0.1)
+C = plt.contour(testx, testy, zi, 6, colors='k',linewidths=1.0)
+plt.clabel(C, inline=0.1, fontsize=9)
 
 
 plt.show()
