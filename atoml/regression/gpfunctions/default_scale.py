Fix bug in default scaling.

atoml/regression/gpfunctions/default_scale.py
@@ -1,17 +1,17 @@
 """Class to scale everything within regression functions."""
+import numpy as np
+
 from atoml.preprocess.feature_preprocess import standardize
 from atoml.preprocess.scale_target import target_standardize
 
 class ScaleData(object):
     def __init__(self, train_features, train_targets):
-        self.train_features = train_features
-        self.train_targets = train_targets
+        self.train_features = np.asarray(train_features)
+        self.train_targets = np.asarray(train_targets)
 
     def train(self):
         """Scale the training features and targets."""
-        self.feature_data = standardize(
-            train_matrix=self.train_features, mean=None, std=None, local=True
-            )
+        self.feature_data = standardize(train_matrix=self.train_features)
 
         self.target_data = target_standardize(target=self.train_targets)
 
@@ -25,8 +25,9 @@ class ScaleData(object):
         test_features : array
             Feature matrix for the test data.
         """
-        scaled = (test_features - self.feature_data['mean']) \
-            * self.feature_data['std']
+        test_features = np.asarray(test_features)
+        center = test_features - self.feature_data['mean']
+        scaled = center / self.feature_data['std']
 
         return scaled
 
@@ -38,6 +39,7 @@ class ScaleData(object):
         predictions : list
            The predicted values from the GP.
         """
-        p = predictions * self.target_data['std'] + self.target_data['mean']
+        predictions = np.asarray(predictions)
+        p = (predictions * self.target_data['std']) + self.target_data['mean']
 
         return p