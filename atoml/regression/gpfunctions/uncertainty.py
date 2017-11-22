9cf57edcb18a6ac08ee5297165fe82956fb8c59a
Paul C. Jennings <jennings.p.c@gmail.com>
2017-11-22 09:47:56 -0800

    Add more docs to uncertainty and flag for noise term.

--- atoml/regression/gpfunctions/uncertainty.py
+++ atoml/regression/gpfunctions/uncertainty.py
@@ -7,7 +7,8 @@ import numpy as np
 from .covariance import get_covariance
 
 
-def get_uncertainty(kernel_dict, test_fp, reg, ktb, cinv, log_scale):
+def get_uncertainty(kernel_dict, test_fp, reg, ktb, cinv, log_scale,
+                    include_noise):
     """Function to calculate uncertainty.
 
     Parameters
@@ -24,10 +25,24 @@ def get_uncertainty(kernel_dict, test_fp, reg, ktb, cinv, log_scale):
         Covariance matrix for training dataset.
     log_scale : boolean
         Flag to define if the hyperparameters are log scale.
+    include_noise : boolean
+        Flag to determine whether to calculate the uncertainty on the full GP,
+        including the noise on the data. Default is True.
+
+    Returns
+    -------
+    uncertainty : list
+        The uncertainty on each prediction in the test data. By default, this
+        includes a measure of the noise on the data.
     """
+    # Set noise to zero if this shouldn't be accounted for.
+    if not include_noise:
+        reg = 0.
+
     # Generate the test covariance matrix.
     kxx = get_covariance(kernel_dict=kernel_dict,
                          matrix1=test_fp, log_scale=log_scale)
+
     # Calculate the prediction variance for test data.
     scale = np.diagonal(kxx)
     var = np.einsum("ij,ij->i", np.dot(ktb, cinv), ktb)
