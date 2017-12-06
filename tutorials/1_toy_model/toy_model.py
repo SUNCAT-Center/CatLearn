b016ec70bb2061a6a46e17fc96ac020f8ea0acfe
Paul C. Jennings <jennings.p.c@gmail.com>
2017-12-06 10:09:57 -0800

    Fix conflicts with tutorial_1.

--- tutorials/1_toy_model/toy_model.py
+++ tutorials/1_toy_model/toy_model.py
@@ -24,24 +24,22 @@ def afunc(x):
 
 # Setting up data.
 # A number of training points in x.
-train_points = 30
+train_points = 17
 noise_magnitude = 1.
 
 # Randomly generate the training datapoints x.
-train = 7.6 * np.random.random_sample((1, train_points)) - 4.2 + 50
+train = 7.6 * np.random.sample((train_points, 1)) - 4.2 + 50
 # Each element in the list train can be referred to as a fingerprint.
 # Call the underlying function to produce the target values.
 target = np.array(afunc(train))
 
 # Add random noise from a normal distribution to the target values.
-nt = []
-for i in range(train_points):
-    nt.append(noise_magnitude * np.random.normal())
-target += np.array(nt)
+target += noise_magnitude * np.random.randn(train_points, 1)
 
 # Generate test datapoints x.
-test_points = 1001
-test = np.linspace(np.min(train)-0.1, np.max(train)+0.1, test_points)
+test_points = 513
+test = np.vstack(np.linspace(np.min(train)-0.1, np.max(train)+0.1,
+                             test_points))
 
 # Store standard deviations of the training data and targets.
 stdx = np.std(train)
@@ -49,137 +47,166 @@ stdy = np.std(target)
 tstd = 2.
 
 # Standardize the training and test data on the same scale.
-std = standardize(train_matrix=np.reshape(train, (np.shape(train)[1], 1)),
-                  test_matrix=np.reshape(test, (test_points, 1)))
+std = standardize(train_matrix=train,
+                  test_matrix=test)
 # Standardize the training targets.
-train_targets = target_standardize(target[0])
+train_targets = target_standardize(target)
 # Note that predictions will now be made on the standardized scale.
 
-# Model example 1 - biased model.
-# Define prediction parameters.
-sdt1 = np.sqrt(1e-1)
-w1 = 3.0  # Too large widths results in a biased model.
-kdict = {'k1': {'type': 'gaussian', 'width': w1}}
-# Set up the prediction routine.
-gp = GaussianProcess(kernel_dict=kdict, regularization=sdt1**2,
-                     train_fp=std['train'],
-                     train_target=train_targets['target'],
-                     optimize_hyperparameters=False)
-# Do predictions.
-under_fit = gp.predict(test_fp=std['test'], uncertainty=True)
-# Scale predictions back to the original scale.
-under_prediction = np.array(under_fit['prediction']) * train_targets['std'] + \
-    train_targets['mean']
-under_uncertainty = np.array(under_fit['uncertainty']) * train_targets['std']
-# Get average errors.
-error = get_error(under_prediction, afunc(test))
-print('Gaussian linear regression prediction:', error['absolute_average'])
-# Get confidence interval on predictions.
-upper = under_prediction + under_uncertainty * tstd
-lower = under_prediction - under_uncertainty * tstd
-
-# Model example 2 - over-fitting.
-# Define prediction parameters
-sdt2 = np.sqrt(1e-6)
-w2 = 0.03  # Too small widths lead to over-fitting.
-kdict = {'k1': {'type': 'gaussian', 'width': w2}}
-# Set up the prediction routine.
-gp = GaussianProcess(kernel_dict=kdict, regularization=sdt2**2,
-                     train_fp=std['train'],
-                     train_target=train_targets['target'],
-                     optimize_hyperparameters=False)
-# Do predictions.
-over_fit = gp.predict(test_fp=std['test'], uncertainty=True)
-# Scale predictions back to the original scale.
-over_prediction = np.array(over_fit['prediction']) * train_targets['std'] + \
-    train_targets['mean']
-over_uncertainty = np.array(over_fit['uncertainty']) * train_targets['std']
-# Get average errors.
-error = get_error(over_prediction, afunc(test))
-print('Gaussian linear regression prediction:', error['absolute_average'])
-# Get confidence interval on predictions.
-over_upper = over_prediction + over_uncertainty * tstd
-over_lower = over_prediction - over_uncertainty * tstd
-
-# Model example 3 - Gaussian Process.
-# Set up the prediction routine and optimize hyperparameters.
-kdict = {'k1': {'type': 'gaussian', 'width': [w1], 'scaling': 0.9}}
-gp = GaussianProcess(kernel_dict=kdict, regularization=sdt1**2,
-                     train_fp=std['train'],
-                     train_target=train_targets['target'],
-                     optimize_hyperparameters=True)
-print('Optimized kernel:', gp.kernel_dict)
-# Do the optimized predictions.
-optimized = gp.predict(test_fp=std['test'], uncertainty=True)
-# Scale predictions back to the original scale.
-opt_prediction = np.array(optimized['prediction']) * train_targets['std'] + \
-    train_targets['mean']
-opt_uncertainty = np.array(optimized['uncertainty']) * train_targets['std']
-# Get average errors.
-error = get_error(opt_prediction, afunc(test))
-print('Gaussian linear regression prediction:', error['absolute_average'])
-# Get confidence interval on predictions.
-opt_upper = opt_prediction + opt_uncertainty * tstd
-opt_lower = opt_prediction - opt_uncertainty * tstd
-
-# Plotting.
 # Store the known underlying function for plotting.
 linex = np.linspace(np.min(test), np.max(test), test_points)
 liney = afunc(linex)
-
+# Plotting.
 fig = plt.figure(figsize=(15, 8))
-
-# Example 1
-ax = fig.add_subplot(221)
-ax.plot(linex, liney, '-', lw=1, color='black')
-ax.plot(train[0], target[0], 'o', alpha=0.2, color='black')
-ax.plot(test, under_prediction, 'b-', lw=1, alpha=0.4)
-ax.fill_between(test, upper, lower, interpolate=True, color='blue',
-                alpha=0.2)
-plt.title('Biased kernel regression model.  \n' +
-          'w: {0:.3f}, r: {1:.3f}'.format(w1 * stdx, sdt1 * stdy))
-plt.xlabel('Descriptor')
-plt.ylabel('Response')
-plt.axis('tight')
-
-# Example 2
-ax = fig.add_subplot(222)
-ax.plot(linex, liney, '-', lw=1, color='black')
-ax.plot(train[0], target[0], 'o', alpha=0.2, color='black')
-ax.plot(test, over_prediction, 'r-', lw=1, alpha=0.4)
-ax.fill_between(test, over_upper, over_lower, interpolate=True, color='red',
-                alpha=0.2)
-plt.title('Over-fitting kernel regression. \n' +
-          'w: {0:.3f}, r: {1:.3f}'.format(w2 * stdx, sdt2 * stdy))
-plt.xlabel('Descriptor')
-plt.ylabel('Response')
-plt.axis('tight')
-
-# Example 3
-ax = fig.add_subplot(223)
-ax.plot(linex, liney, '-', lw=1, color='black')
-ax.plot(train[0], target[0], 'o', alpha=0.2, color='black')
-ax.plot(test, opt_prediction, 'g-', lw=1, alpha=0.4)
-ax.fill_between(test, opt_upper, opt_lower, interpolate=True, color='green',
-                alpha=0.2)
-plt.title('Optimized GP. \n w: {0:.3f}, r: {1:.3f}'.format(
-    gp.kernel_dict['k1']['width'][0]*stdx, np.sqrt(gp.regularization)*stdy))
-plt.xlabel('Descriptor')
-plt.ylabel('Response')
-plt.axis('tight')
-
-# Uncertainty profile.
-ax = fig.add_subplot(224)
-ax.plot(test, np.array(under_uncertainty * tstd), '-', lw=1,
-        color='blue')
-ax.plot(test, np.array(over_uncertainty * tstd), '-', lw=1,
-        color='red')
-ax.plot(test, np.array(opt_uncertainty * tstd), '-', lw=1,
-        color='green')
+ax224 = fig.add_subplot(224)
 plt.title('Uncertainty Profiles')
 plt.xlabel('Descriptor')
 plt.ylabel('Uncertainty')
-plt.axis('tight')
+
+if True:
+    # Model example 1 - biased model.
+    # Define prediction parameters.
+    sdt1 = 0.001
+    # Too large width results in a biased model.
+    w1 = 3.0
+    kdict = {'k1': {'type': 'gaussian', 'width': w1}}
+    # Set up the prediction routine.
+    gp = GaussianProcess(kernel_dict=kdict, regularization=sdt1**2,
+                         train_fp=std['train'],
+                         train_target=train_targets['target'],
+                         optimize_hyperparameters=False)
+    # Do predictions.
+    under_fit = gp.predict(test_fp=std['test'], uncertainty=True)
+    # Scale predictions back to the original scale.
+    under_prediction = np.vstack(under_fit['prediction']) * \
+        train_targets['std'] + train_targets['mean']
+    under_uncertainty = np.vstack(under_fit['uncertainty']) * \
+        train_targets['std']
+    # Get average errors.
+    error = get_error(under_prediction, afunc(test))
+    print('Gaussian linear regression prediction:', error['absolute_average'])
+    # Get confidence interval on predictions.
+    upper = under_prediction + under_uncertainty * tstd
+    lower = under_prediction - under_uncertainty * tstd
+
+    # Plot example 1
+    ax = fig.add_subplot(221)
+    ax.plot(linex, liney, '-', lw=1, color='black')
+    ax.plot(train, target, 'o', alpha=0.2, color='black')
+    ax.plot(test, under_prediction, 'b-', lw=1, alpha=0.4)
+    ax.fill_between(np.hstack(test), np.hstack(upper), np.hstack(lower),
+                    interpolate=True, color='blue',
+                    alpha=0.2)
+    plt.title('Biased kernel regression model.  \n' +
+              'w: {0:.3f}, r: {1:.3f}'.format(w1 * stdx,
+                                              sdt1 * stdy))
+    plt.xlabel('Descriptor')
+    plt.ylabel('Response')
+    plt.axis('tight')
+
+if True:
+    # Model example 2 - over-fitting.
+    # Define prediction parameters
+    sdt2 = 0.001
+    # Too small width lead to over-fitting.
+    w2 = 0.03
+    kdict = {'k1': {'type': 'gaussian', 'width': w2}}
+    # Set up the prediction routine.
+    gp = GaussianProcess(kernel_dict=kdict, regularization=sdt2**2,
+                         train_fp=std['train'],
+                         train_target=train_targets['target'],
+                         optimize_hyperparameters=False)
+    # Do predictions.
+    over_fit = gp.predict(test_fp=std['test'], uncertainty=True)
+    # Scale predictions back to the original scale.
+    over_prediction = np.vstack(over_fit['prediction']) * \
+        train_targets['std'] + train_targets['mean']
+    over_uncertainty = np.vstack(over_fit['uncertainty']) * \
+        train_targets['std']
+    # Get average errors.
+    error = get_error(over_prediction, afunc(test))
+    print('Gaussian kernel regression prediction:', error['absolute_average'])
+    # Get confidence interval on predictions.
+    over_upper = over_prediction + over_uncertainty * tstd
+    over_lower = over_prediction - over_uncertainty * tstd
+
+    # Uncertainty profile.
+    ax224.plot(test, np.array(under_uncertainty * tstd), '-', lw=1,
+               color='blue')
+
+    # Plot example 2
+    ax = fig.add_subplot(222)
+    ax.plot(linex, liney, '-', lw=1, color='black')
+    ax.plot(train, target, 'o', alpha=0.2, color='black')
+    ax.plot(test, over_prediction, 'r-', lw=1, alpha=0.4)
+    ax.fill_between(np.hstack(test), np.hstack(over_upper),
+                    np.hstack(over_lower), interpolate=True, color='red',
+                    alpha=0.2)
+    plt.title('Over-fitting kernel regression. \n' +
+              'w: {0:.3f}, r: {1:.3f}'.format(w2 * stdx,
+                                              sdt2 * stdy))
+    plt.xlabel('Descriptor')
+    plt.ylabel('Response')
+    plt.axis('tight')
+    # Uncertainty profile.
+    ax224.plot(test, np.array(over_uncertainty * tstd), '-', lw=1,
+               color='red')
+
+if True:
+    # Model example 3 - Gaussian Process.
+    # Set up the prediction routine and optimize hyperparameters.
+    w3 = 0.1
+    sdt3 = 0.001
+    kdict = {
+             'k1': {'type': 'gaussian', 'width': [w3]},
+             }
+    gp = GaussianProcess(kernel_dict=kdict, regularization=sdt3**2,
+                         train_fp=std['train'],
+                         train_target=train_targets['target'],
+                         optimize_hyperparameters=True)
+    print('Optimized kernel:', gp.kernel_dict)
+    print(-gp.theta_opt['fun'])
+    # Do the optimized predictions.
+    optimized = gp.predict(test_fp=std['test'], uncertainty=True)
+    # Scale predictions back to the original scale.
+    opt_prediction = np.vstack(optimized['prediction']) * \
+        train_targets['std'] + train_targets['mean']
+    opt_uncertainty = np.vstack(optimized['uncertainty']) * \
+        train_targets['std']
+    # Get average errors.
+    error = get_error(opt_prediction, afunc(test))
+    print('Gaussian kernel regression prediction:', error['absolute_average'])
+    # Get confidence interval on predictions.
+    opt_upper = opt_prediction + opt_uncertainty * tstd
+    opt_lower = opt_prediction - opt_uncertainty * tstd
+
+    # Plot eample 3
+    ax = fig.add_subplot(223)
+    ax.plot(linex, liney, '-', lw=1, color='black')
+    ax.plot(train, target, 'o', alpha=0.2, color='black')
+    ax.plot(test, opt_prediction, 'g-', lw=1, alpha=0.4)
+    ax.fill_between(np.hstack(test), np.hstack(opt_upper),
+                    np.hstack(opt_lower), interpolate=True,
+                    color='green', alpha=0.2)
+    plt.title('Optimized GP. \n w: {0:.3f}, r: {1:.3f}'.format(
+        gp.kernel_dict['k1']['width'][0]*stdx,
+        np.sqrt(gp.regularization)*stdy))
+    plt.xlabel('Descriptor')
+    plt.ylabel('Response')
+    plt.axis('tight')
+    # Uncertainty profile.
+    ax224.plot(test, np.array(opt_uncertainty * tstd), '-', lw=1,
+               color='green')
+
+if False:
+    # Model example 4 - GPy.
+    import GPy
+    # Set up the prediction routine and optimize hyperparameters.
+    kernel = GPy.kern.RBF(input_dim=1, variance=1., lengthscale=1.)
+    m = GPy.models.GPRegression(std['train'], train_targets['target'], kernel)
+    m.optimize()
+    print(m)
+
 plt.subplots_adjust(left=None, bottom=None, right=None, top=None,
                     wspace=0.4, hspace=0.4)
 plt.show()
