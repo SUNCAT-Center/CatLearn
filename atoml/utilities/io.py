5b3d1fdf1b3c97767f61cd7bf920787c91460317
Paul C. Jennings <jennings.p.c@gmail.com>
2018-01-28 15:50:40 -0800

    Add docs to io.

--- atoml/utilities/io.py
+++ atoml/utilities/io.py
@@ -3,12 +3,31 @@ import pickle
 
 
 def write(filename, model):
-    """Function to write a pickle of model object."""
+    """Function to write a pickle of model object.
+
+    Parameters
+    ----------
+    filename : str
+        The name of the save file.
+    model : obj
+        Python GaussianProcess object.
+    """
     with open('{}.pkl'.format(filename), 'wb') as outfile:
         pickle.dump(model, outfile, pickle.HIGHEST_PROTOCOL)
 
 
 def read(filename):
-    """Function to read a pickle of model object."""
+    """Function to read a pickle of model object.
+
+    Parameters
+    ----------
+    filename : str
+        The name of the save file.
+
+    Returns
+    -------
+    model : obj
+        Python GaussianProcess object.
+    """
     with open('{}.pkl'.format(filename), 'rb') as infile:
         return pickle.load(infile)
