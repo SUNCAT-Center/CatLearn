f84e431b8e00a62fd1577a2183def46ecacc0e40
Paul C. Jennings <jennings.p.c@gmail.com>
2018-02-02 08:19:53 -0800

    Better api tests.

--- test/test_ase_api.py
+++ test/test_ase_api.py
@@ -3,10 +3,12 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 import os
+import numpy as np
 
 from ase.ga.data import DataConnection
 
 from atoml.api.ase_api import extend_class
+from atoml.fingerprint import StandardFingerprintGenerator
 
 wkdir = os.getcwd()
 
@@ -15,10 +17,18 @@ def ase_api_test():
     """Test the ase api."""
     gadb = DataConnection('{}/data/gadb.db'.format(wkdir))
     all_cand = gadb.get_all_relaxed_candidates()
+
+    cf = all_cand[0].get_chemical_formula()
+
     extend_class(all_cand[0])
-    all_cand[0].set_features([1., 1., 2., 1.])
-    assert all_cand[0].get_features() == [1., 1., 2., 1.]
-    assert all_cand[0].get_chemical_formula() == 'Au92Pt55'
+    assert isinstance(all_cand[0], type(all_cand[1]))
+
+    sf = StandardFingerprintGenerator()
+    fp = sf.composition_fpv(all_cand[0])
+    all_cand[0].set_features(fp)
+
+    assert np.allclose(all_cand[0].get_features(), fp)
+    assert all_cand[0].get_chemical_formula() == cf
 
 
 if __name__ == '__main__':
