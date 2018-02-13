376debd5f5105138cc42cd72f13a62ef28d90463
Paul C. Jennings <jennings.p.c@gmail.com>
2018-02-12 23:11:42 -0800

    Suppress some warnings in the tests for cleaner output.

--- test/test_suite.py
+++ test/test_suite.py
@@ -19,20 +19,22 @@ import test_learning_curve as tlc
 
 from common import get_data
 
-warnings.filterwarnings("ignore")
-
 
 class ConfigTestCase(unittest.TestCase):
     """Test suite for AtoML code base."""
 
     def test_ads_fp_funct(self):
         """Test adsorbate fingerprinting functions."""
+        warnings.filterwarnings("ignore")
+
         images = afp.setup_atoms()
         images = afp.attach_adsorbate_info(images)
         afp.ads_fg_gen(images)
 
     def test_data_setup_func(self):
         """Test data setup routines."""
+        warnings.filterwarnings("ignore")
+
         # Test data setup functions.
         fb.feature_base_test()
         all_cand, data = ds.feature_test()
@@ -50,12 +52,16 @@ class ConfigTestCase(unittest.TestCase):
 
     def test_data_clean_func(self):
         """Test data cleaning routines."""
+        warnings.filterwarnings("ignore")
+
         dc.outlier_test()
         dc.variance_test()
         dc.inf_test()
 
     def test_feature_opt_func(self):
         """Test feature optimization routines."""
+        warnings.filterwarnings("ignore")
+
         ft.test_importance()
         train_features, train_targets, test_features = ft.test_extend()
         ft.test_extract(train_features, train_targets, test_features)
@@ -66,6 +72,8 @@ class ConfigTestCase(unittest.TestCase):
 
     def test_predict_func(self):
         """Test prediction routines."""
+        warnings.filterwarnings("ignore")
+
         train_features, train_targets, test_features, \
             test_targets = get_data()
         pt.rr_test(train_features, train_targets, test_features, test_targets)
@@ -74,12 +82,16 @@ class ConfigTestCase(unittest.TestCase):
 
     def test_lml_optimizer(self):
         """Test log_marginal_likelihood optimization."""
+        warnings.filterwarnings("ignore")
+
         train_features, train_targets, test_features, \
             test_targets = get_data()
         lo.lml_test(train_features, train_targets, test_features, test_targets)
 
     def test_acquisition_func(self):
         """Test acquisition routines."""
+        warnings.filterwarnings("ignore")
+
         train_features, train_targets, train_atoms, test_features, \
             test_targets, test_atoms = ta.get_data()
         ta.gp_test(train_features, train_targets, train_atoms,
@@ -87,12 +99,16 @@ class ConfigTestCase(unittest.TestCase):
 
     def test_hierarchy_func(self):
         """Test hierarchy routines."""
+        warnings.filterwarnings("ignore")
+
         cv.hierarchy_test()
         cv.kfold_test()
         tlc.learning_curve_test()
 
     def test_io_func(self):
         """Test the io routines."""
+        warnings.filterwarnings("ignore")
+
         train_features, train_targets, test_features, \
             test_targets = get_data()
         model = tio.train_model(train_features, train_targets)
