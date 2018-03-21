80b8805ee79a6a62c62a48e3aeeae125685a6889
Ziyun Wang <ziyunwang@live.com>
2018-03-21 11:41:16 -0700

    removed property passing in Voronoi FP

--- atoml/fingerprint/voro_fingerprint.py
+++ atoml/fingerprint/voro_fingerprint.py
@@ -12,8 +12,7 @@ class VoronoiFingerprintGenerator(object):
     Based on a list of ase.atoms object.
     """
 
-    def __init__(self, atoms, system_name='', target='id', delete_temp=True,
-                 properties=[]):
+    def __init__(self, atoms, delete_temp=True):
         """Initialize voronoi generator.
 
         Parameters
@@ -22,16 +21,13 @@ class VoronoiFingerprintGenerator(object):
             list of structures in ase.atoms.
         """
         self.atoms = atoms
-        self.system_name = system_name
-        self.temp_path = 'voro_temp' + system_name
+        self.temp_path = 'voro_temp'
         from atoml import __path__
         if os.path.exists(__path__[0] + '/api/magpie'):
             self.magpie_path = __path__[0] + '/api/magpie'
         else:
             raise EnvironmentError('Magpie path not exist!')
         self.cif_path = self.temp_path + '/cif/'
-        self.target = target
-        self.properties = properties
         self.voro_input = '''data = new data.materials.CrystalStructureDataset
 data attributes properties directory %s/lookup-data
 data attributes properties add set general
@@ -39,7 +35,7 @@ data import ./%s
 data target %s
 data attributes generate
 save data %s/voro_FP csv
-exit''' % (self.magpie_path, self.cif_path, self.target, self.temp_path)
+exit''' % (self.magpie_path, self.cif_path, 'id', self.temp_path)
         self.magpie = 'java -jar %s/Magpie.jar' % self.magpie_path
         self.delete_temp = delete_temp
 
@@ -55,8 +51,6 @@ exit''' % (self.magpie_path, self.cif_path, self.target, self.temp_path)
         i = 0
         filename = []
         pro_dict = {}
-        for pro in self.properties:
-            pro_dict[pro] = []
         fml = []
         id_list = []
         for at in self.atoms:
@@ -65,18 +59,13 @@ exit''' % (self.magpie_path, self.cif_path, self.target, self.temp_path)
                      format='vasp', direct=True, vasp5=True)
             filename.append(at_name)
             fml.append(at.get_chemical_formula())
-            for pro in self.properties:
-                if hasattr(at, pro):
-                    pro_dict[pro].append(getattr(at, pro))
-                else:
-                    pro_dict[pro].append(None)
             id_list.append(i)
             i += 1
         pro_dict['filename'] = filename
         pro_dict['formula'] = fml
         pro_dict['id'] = id_list
         temp_pd = pd.DataFrame.from_dict(pro_dict)
-        temp_pd = temp_pd[['filename', 'id', 'formula'] + self.properties]
+        temp_pd = temp_pd[['filename', 'id', 'formula']]
         self.input_pd = temp_pd
         temp_pd.to_csv(self.cif_path + 'properties.txt', sep=' ', index=False)
         f = open(self.temp_path + '/voro_FP.in', 'w')
@@ -107,9 +96,4 @@ exit''' % (self.magpie_path, self.cif_path, self.target, self.temp_path)
         if self.delete_temp:
             import shutil
             shutil.rmtree(self.temp_path)
-        return FP
-
-    def generate_all(self):
-        self.target = 'id'
-        temp_FP = self.generate()
-        return pd.merge(self.input_pd, temp_FP, left_on='id', right_on='id')
+        return pd.merge(self.input_pd, FP, left_on='id', right_on='id').drop(['filename','formula','id'], axis=1)
