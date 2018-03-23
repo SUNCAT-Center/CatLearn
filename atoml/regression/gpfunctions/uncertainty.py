885c945ba91bd122cc066c6f087da1ebf6e4d0e9
Jose A Garrido Torres <jagt@stanford.edu>
2018-03-23 15:40:19 -0700

    The user can ask for the uncertainty on the variance ['uncertainty'] or including the noise parameter ['uncertainty_with_reg'].

--- atoml/regression/gpfunctions/uncertainty.py
+++ atoml/regression/gpfunctions/uncertainty.py
@@ -7,8 +7,7 @@ import numpy as np
 from .covariance import get_covariance
 
 
-def get_uncertainty(kernel_dict, test_fp, reg, ktb, cinv, log_scale,
-                    include_noise=True):
+def get_uncertainty(kernel_dict, test_fp, ktb, cinv, log_scale):
     """Function to calculate uncertainty.
 
     Parameters
@@ -17,17 +16,12 @@ def get_uncertainty(kernel_dict, test_fp, reg, ktb, cinv, log_scale,
         Dictionary containing all information for the kernels.
     test_fp : array
         Test feature set.
-    reg : float
-        Regularization parameter.
     ktb : array
         Covariance matrix for test and training data.
     cinv : array
         Covariance matrix for training dataset.
     log_scale : boolean
         Flag to define if the hyperparameters are log scale.
-    include_noise : boolean
-        Flag to determine whether to calculate the uncertainty on the full GP,
-        including the noise on the data. Default is True.
 
     Returns
     -------
@@ -35,9 +29,6 @@ def get_uncertainty(kernel_dict, test_fp, reg, ktb, cinv, log_scale,
         The uncertainty on each prediction in the test data. By default, this
         includes a measure of the noise on the data.
     """
-    # Set noise to zero if this shouldn't be accounted for.
-    if not include_noise:
-        reg = 0.
 
     # Generate the test covariance matrix.
     kxx = get_covariance(
@@ -48,6 +39,6 @@ def get_uncertainty(kernel_dict, test_fp, reg, ktb, cinv, log_scale,
     # Calculate the prediction variance for test data.
     scale = np.diagonal(kxx)
     var = np.einsum("ij,ij->i", np.dot(ktb, cinv), ktb)
-    uncertainty = np.sqrt(reg + scale - var)
+    uncertainty = np.sqrt(scale - var)
 
     return uncertainty
