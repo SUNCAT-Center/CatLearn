9c2180d6c93a54fc56d8cc2ccc701cf2fe1619a8
mhangaard <mhangaard@gmail.com>
2018-04-26 13:43:01 -0700

    more testing

--- test/test_ads_fp_gen.py
+++ test/test_ads_fp_gen.py
@@ -9,6 +9,7 @@ import unittest
 from ase.build import fcc111, add_adsorbate
 from ase.data import atomic_numbers
 from ase.constraints import FixAtoms
+from catlearn.api.ase_atoms_api import database_to_list
 from catlearn.fingerprint.adsorbate_prep import autogen_info
 from catlearn.fingerprint.periodic_table_data import (get_radius,
                                                       default_catlearn_radius)
@@ -31,16 +32,13 @@ class TestAdsorbateFeatures(unittest.TestCase):
             for ads in adsorbates:
                 atoms = fcc111(s, (2, 2, 3), a=a)
                 atoms.center(vacuum=6, axis=2)
-                c_atoms = [a.index for a in atoms if
-                           a.z < atoms.cell[2, 2] / 2. + 0.1]
-                atoms.set_constraint(FixAtoms(c_atoms))
                 h = (default_catlearn_radius(
                     atomic_numbers[ads]) + rs) / 2 ** 0.5
                 add_adsorbate(atoms, ads, h, 'bridge')
                 images.append(atoms)
         return images
 
-    def test_ads_fp_gen(self):
+    def test_raw_ads(self):
         """Test the feature generation."""
         images = self.setup_atoms()
         images = autogen_info(images)
@@ -58,9 +56,68 @@ class TestAdsorbateFeatures(unittest.TestCase):
                      gen.strain,
                      gen.en_difference_ads,
                      gen.en_difference_chemi,
-                     # gen.ads_av,
-                     # gen.ads_sum,
                      ]
+        matrix = gen.return_vec(images, train_fpv)
+        labels = gen.return_names(train_fpv)
+        print(np.shape(matrix), type(matrix))
+        if __name__ == '__main__':
+            for i, l in enumerate(labels):
+                print(i, l)
+        self.assertTrue(len(labels) == np.shape(matrix)[1])
+
+    def test_constrained_ads(self):
+        """Test the feature generation."""
+        images = self.setup_atoms()
+        for atoms in images:
+            c_atoms = [a.index for a in atoms if
+                       a.z < atoms.cell[2, 2] / 2. + 0.1]
+            atoms.set_constraint(FixAtoms(c_atoms))
+        images = autogen_info(images)
+        print(str(len(images)) + ' training examples.')
+        gen = FeatureGenerator(nprocs=None)
+        train_fpv = [gen.mean_chemisorbed_atoms,
+                     gen.count_chemisorbed_fragment,
+                     gen.count_ads_atoms,
+                     gen.count_ads_bonds,
+                     gen.mean_site,
+                     gen.sum_site,
+                     gen.mean_surf_ligands,
+                     gen.term,
+                     gen.bulk,
+                     gen.strain,
+                     gen.en_difference_ads,
+                     gen.en_difference_chemi,
+                     ]
+        matrix = gen.return_vec(images, train_fpv)
+        labels = gen.return_names(train_fpv)
+        print(np.shape(matrix), type(matrix))
+        if __name__ == '__main__':
+            for i, l in enumerate(labels):
+                print(i, l)
+        self.assertTrue(len(labels) == np.shape(matrix)[1])
+
+    def test_db_ads(self):
+        """Test the feature generation."""
+        images = database_to_list('data/ads_example.db')
+        images = autogen_info(images)
+        print(str(len(images)) + ' training examples.')
+        gen = FeatureGenerator(nprocs=1)
+        train_fpv = [gen.mean_chemisorbed_atoms,
+                     gen.count_chemisorbed_fragment,
+                     gen.count_ads_atoms,
+                     gen.count_ads_bonds,
+                     gen.mean_site,
+                     gen.sum_site,
+                     gen.mean_surf_ligands,
+                     gen.term,
+                     gen.bulk,
+                     gen.strain,
+                     gen.en_difference_ads,
+                     gen.en_difference_chemi,
+                     gen.db_size,
+                     gen.ctime,
+                     gen.dbid,
+                     gen.delta_energy]
         matrix = gen.return_vec(images, train_fpv)
         labels = gen.return_names(train_fpv)
         print(np.shape(matrix), type(matrix))
