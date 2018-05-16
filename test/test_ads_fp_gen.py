e6ac716fa92de8715bfc7c556a60a058deb48a68
mhangaard <mhangaard@gmail.com>
2018-05-16 09:01:14 -0700

    test check reconstructions

--- test/test_ads_fp_gen.py
+++ test/test_ads_fp_gen.py
@@ -10,7 +10,9 @@ from ase.build import fcc111, add_adsorbate
 from ase.data import atomic_numbers
 from ase.constraints import FixAtoms
 from catlearn.api.ase_atoms_api import database_to_list
-from catlearn.fingerprint.adsorbate_prep import autogen_info
+from catlearn.fingerprint.adsorbate_prep import (autogen_info,
+                                                 check_reconstructions,
+                                                 images_connectivity)
 from catlearn.fingerprint.periodic_table_data import (get_radius,
                                                       default_catlearn_radius)
 from catlearn.fingerprint.setup import FeatureGenerator, default_fingerprinters
@@ -90,6 +92,20 @@ class TestAdsorbateFeatures(unittest.TestCase):
             for i, l in enumerate(labels):
                 print(i, l)
         self.assertTrue(len(labels) == np.shape(matrix)[1])
+
+    def test_recontruction(self):
+        images = self.setup_atoms()
+        slabs = []
+        for atoms in images:
+            slab = atoms.copy()
+            slab.pop(-1)
+            slabs.append(slab)
+        images = autogen_info(images)
+        slabs = images_connectivity(slabs)
+        image_pairs = zip(images, slabs)
+        reconstructed = check_reconstructions(image_pairs)
+
+        self.assertTrue(len(reconstructed) == 0)
 
 
 if __name__ == '__main__':
