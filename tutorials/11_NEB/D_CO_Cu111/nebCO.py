d772b5292d3f2135316b3f6dd78c38b37129ed20
Jose A Garrido Torres <jagt@stanford.edu>
2018-07-24 21:11:50 -0700

    New toy models added.

---
+++ tutorials/11_NEB/D_CO_Cu111/nebCO.py
@@ -0,0 +1,105 @@
+from math import sqrt
+from ase import Atoms, Atom
+from ase.calculators.emt import EMT
+from ase.constraints import FixAtoms
+from ase.optimize import BFGS, QuasiNewton
+from ase.neb import NEB
+from ase.io import Trajectory
+from ase.visualize import view
+from ase.io import read, write
+from catlearn.optimize.catlearn_neb_optimizer import CatLearnNEB
+from catlearn.optimize.catlearn_autoneb_optimizer import CatLearnAutoNEB
+import matplotlib.pyplot as plt
+
+from ase.neb import NEBTools
+
+import copy
+
+# Calculator:
+ase_calculator = EMT()
+n_images = 6
+
+# Distance between Cu atoms on a (111) surface:
+a = 3.6
+d = a / sqrt(2)
+fcc111 = Atoms(symbols='Cu',
+               cell=[(d, 0, 0),
+                     (d / 2, d * sqrt(3) / 2, 0),
+                     (d / 2, d * sqrt(3) / 6, -a / sqrt(3))],
+               pbc=True)
+slab = fcc111 * (2, 2, 4)
+slab.set_cell([2 * d, d * sqrt(3), 1])
+slab.set_pbc((1, 1, 0))
+slab.calc = EMT()
+Z = slab.get_positions()[:, 2]
+indices = [i for i, z in enumerate(Z) if z < Z.mean()]
+constraint = FixAtoms(indices=indices)
+slab.set_constraint(constraint)
+dyn = QuasiNewton(slab)
+dyn.run(fmax=0.01)
+Z = slab.get_positions()[:, 2]
+
+
+b = 1.2
+h = 1.5
+slab += Atom('C', (d / 2, -b / 2, h))
+slab += Atom('O', (d / 2, +b / 2, h))
+s = slab.copy()
+dyn = QuasiNewton(slab)
+dyn.run(fmax=0.01)
+
+# Make band:
+images = [slab]
+for i in range(n_images):
+    image = slab.copy()
+    # Set constraints and calculator:
+    image.set_constraint(constraint)
+    image.calc = copy.deepcopy(ase_calculator)
+    images.append(image)
+
+# Displace last image:
+image[-2].position = image[-1].position
+image[-1].x = d
+image[-1].y = d / sqrt(3)
+
+dyn = QuasiNewton(images[-1])
+dyn.run(fmax=0.05)
+neb = NEB(images, climb=True)
+
+# Interpolate positions between initial and final states:
+neb.interpolate(method='idpp')
+
+for image in images:
+    print(image.positions[-1], image.get_potential_energy())
+
+dyn = BFGS(neb, maxstep=0.04, trajectory='mep.traj')
+dyn.run(fmax=0.05)
+
+for image in images:
+    print(image.positions[-1], image.get_potential_energy())
+
+nebtools_ase = NEBTools(images)
+
+Sf_ase = nebtools_ase.get_fit()[2]
+Ef_ase = nebtools_ase.get_fit()[3]
+
+Ef_neb_ase, dE_neb_ase = nebtools_ase.get_barrier(fit=False)
+nebtools_ase.plot_band()
+
+plt.show()
+
+# Using CatLearn:
+
+write('initial.traj', images[0])
+
+write('final.traj', images[-1])
+
+
+neb_catlearn = CatLearnNEB(start='initial.traj',
+                           end='final.traj',
+                           ase_calc=copy.deepcopy(ase_calculator),
+                           n_images=n_images,
+                           interpolation='idpp', restart=False)
+
+neb_catlearn.run(fmax=0.05, plot_neb_paths=True, acquisition='acq_1')
+
---
+++ tutorials/11_NEB/E_terrace_pt/nebterrace.py
@@ -0,0 +1,83 @@
+from ase.build import fcc100, add_adsorbate
+from ase.calculators.emt import EMT
+from ase.io import read
+from ase.constraints import FixAtoms
+from ase.neb import NEB
+from ase.optimize import BFGS, MDMin, FIRE
+import matplotlib.pyplot as plt
+from catlearn.optimize.catlearn_neb_optimizer import CatLearnNEB
+from catlearn.optimize.catlearn_autoneb_optimizer import CatLearnAutoNEB
+
+from ase.neb import NEBTools
+import copy
+from ase.build import surface
+from ase.visualize import view
+from ase.build import add_adsorbate
+
+""" 
+    Toy model for the diffusion of a Pt atom on an Pt(211) surface.  
+    This example contains: 
+    1) Optimization of the initial and final end-points of the reaction path. 
+    2A) NEB optimization using NEB-CI as implemented in ASE. 
+    2B) NEB optimization using our machine-learning surrogate model.
+    3) Comparison between the ASE NEB and our Machine Learning assisted NEB 
+       algorithm.
+"""
+
+# 1. Structural relaxation. ##################################################
+
+# Setup calculator:
+ase_calculator = EMT()
+
+slab = read('initial.traj')
+slab.set_calculator(copy.deepcopy(ase_calculator))
+qn = BFGS(slab, trajectory='initial_opt.traj')
+qn.run(fmax=0.01)
+
+# Final end-point:
+slab = read('final.traj')
+slab.set_calculator(copy.deepcopy(ase_calculator))
+qn = BFGS(slab, trajectory='final_opt.traj')
+qn.run(fmax=0.01)
+
+
+# # Define number of images:
+n_images = 7
+
+# # 2.A. NEB using ASE #########################################################
+
+initial_ase = read('initial_opt.traj')
+final_ase = read('final_opt.traj')
+
+images_ase = [initial_ase]
+for i in range(1, n_images-1):
+    image = initial_ase.copy()
+    image.set_calculator(copy.deepcopy(ase_calculator))
+    images_ase.append(image)
+images_ase.append(final_ase)
+
+neb_ase = NEB(images_ase, climb=True, method='aseneb')
+neb_ase.interpolate(method='idpp')
+
+qn_ase = MDMin(neb_ase, trajectory='neb_ase.traj')
+qn_ase.run(fmax=0.05)
+
+nebtools_ase = NEBTools(images_ase)
+
+Sf_ase = nebtools_ase.get_fit()[2]
+Ef_ase = nebtools_ase.get_fit()[3]
+
+Ef_neb_ase, dE_neb_ase = nebtools_ase.get_barrier(fit=False)
+nebtools_ase.plot_band()
+
+plt.show()
+
+
+neb_catlearn = CatLearnNEB(start='initial_opt.traj',
+                           end='final_opt.traj',
+                           ase_calc=copy.deepcopy(ase_calculator),
+                           n_images=n_images,
+                           interpolation='idpp', restart=False)
+
+neb_catlearn.run(fmax=0.05, plot_neb_paths=True, acquisition='acq_1',
+                 penalty=3.0)
