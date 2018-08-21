Major changes. 1) Calculator: uncertainty**2 2) Scaling fixed in the hyperparameter optimization. 3) Reg. and width bounds increased. 4) Changed ML optimizer to MDMin.

tutorials/12_structure_relaxation/C_Heptamer_Island/min_heptamer_island.py
@@ -6,6 +6,7 @@ import copy
 from ase.visualize import view
 import numpy as np
 
+
 """ 
     Toy model minimization of Pt heptamer island on Pt(111).
     Minimization example. 
@@ -20,10 +21,11 @@ ase_calculator = EMT()
 
 common_initial = read('./A_structure/POSCAR')
 
+np.random.seed(1)
 for i in common_initial:
     if i.position[2] > 14.00:
         i.symbol = 'Au'
-        i.position = i.position + np.random.normal(scale=0.1)
+        i.position = i.position + np.random.normal(scale=0.2)
 
 # 2.A. Optimize structure using ASE.
 initial_ase = copy.deepcopy(common_initial)