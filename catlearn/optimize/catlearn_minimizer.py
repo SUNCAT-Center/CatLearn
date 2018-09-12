61155cc65d4e66f2ee57568e0276413dd938a1ac
Jose A Garrido Torres <jagt@stanford.edu>
2018-09-12 15:55:58 -0700

    Change in scaling constant

--- catlearn/optimize/catlearn_minimizer.py
+++ catlearn/optimize/catlearn_minimizer.py
@@ -162,7 +162,7 @@ class CatLearnMin(object):
 
             max_target = np.max(self.list_targets)
             scaled_targets = self.list_targets.copy() - max_target
-            scaling = 0.1 + np.std(scaled_targets)**2
+            scaling = 1.0 + np.std(scaled_targets)**2
 
             width = 0.4
             noise_energy = 0.0005
--- catlearn/optimize/catlearn_neb_optimizer.py
+++ catlearn/optimize/catlearn_neb_optimizer.py
@@ -258,7 +258,7 @@ class CatLearnNEB(object):
 
             max_target = np.max(self.list_targets)
             scaled_targets = self.list_targets.copy() - max_target
-            scaling = 0.1 + np.std(scaled_targets)**2
+            scaling = 1.0 + np.std(scaled_targets)**2
 
             width = 0.4
             noise_energy = 0.0005
