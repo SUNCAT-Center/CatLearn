9458b7905d4ab4182b8865f7922bcaa4967959a1
schlexer <p.schlexer@gmail.com>
2018-10-31 15:20:29 -0700

    Removed builder functions, rearranged functions, and added docstrings.

--- catlearn/featurize/slab_utilities.py
+++ catlearn/featurize/slab_utilities.py
@@ -23,44 +23,41 @@ metals = ['Li', 'Be', 'Na', 'Mg', 'K', 'Ca', 'Rb', 'Sr', 'Cs', 'Ba', 'Fr', 'Ra',
           'Cf', 'Es', 'Fm', 'Md', 'No', 'Lw', 'B', 'Al', 'Si', 'Ga', 'Ge', 'In', 'Sn', 'Tl', 'Pb']
 metals.extend(transition_metals)
 
-def auto_kpoints(atoms, effective_length=25, bulk=False):
-    """Return a tuple of k-points derived from the unit cell.
+def stoichiometry(atoms):
+    """Return a number of layers given a slab.
 
     Parameters
     ----------
-        atoms : object
-        effective_length : k-point*unit-cell-parameter
-        bulk : Whether it is a bulk system.
-    """
-    cell = atoms.get_cell()
-    nkx = int(round(effective_length/np.linalg.norm(cell[0]),0))
-    nky = int(round(effective_length/np.linalg.norm(cell[1]),0))
-    if bulk == True:
-        nkz = int(round(effective_length/np.linalg.norm(cell[2]),0))
-    else:
-        nkz = 1
-    return(nkx, nky, nkz)
+    atoms : object
+        ASE atoms object.
 
-def fix_bottom_layers(atoms, top_layers=1):
-    """Return a atoms object with bottom layers fixed
-    Parameters
-    ----------
-        atoms : object
-        top_layers : number of layers to keep free
+    Returns
+    -------
+    num_dict : dictionary
+        First entry is total number of atoms.
+        Then key =  element and entry = number
     """
-    # atom_idx is a list of lists containing the atoms in the layer
-    layer_z, atom_idx = slab_layers(atoms)
-    fixed_atoms = atom_idx[:-1]
-    fixed_atoms = [val for sublist in fixed_atoms for val in sublist]
-    c = FixAtoms(indices = fixed_atoms)
-    atoms.set_constraint([c])
-    return(atoms)
+    symbols = atoms.get_chemical_symbols()
+    num_dict = {}
+    num_dict['total'] = len(symbols)
+    elements = list(set(symbols))
+    for element in elements:
+        count = symbols.count(element)
+        num_dict[element] = count
+    return(num_dict)
 
 def is_metal(chemical_symbol):
     """Checks whether string is a metal elementary symbol.
+
     Parameters
     ----------
-        chemical_symbol : string
+    chemical_symbol : string
+        The element name.
+
+    Returns
+    -------
+    metal : Boolean
+        Whether it's a metal.
     """
     metal = False
     if chemical_symbol in metals:
@@ -68,10 +65,17 @@ def is_metal(chemical_symbol):
     return(metal)
 
 def is_oxide(atoms):
-    """Checks whether a system might be an oxide.
+    """Checks whether atms object is an oxide.
+
     Parameters
     ----------
-        atoms : object.
+    atoms : object
+        ASE atoms object.
+
+    Returns
+    -------
+    oxide : Boolean
+        Whether it is likely an oxide.
     """
     oxide = False
     num_dict = stoichiometry(atoms)
@@ -96,73 +100,29 @@ def is_oxide(atoms):
                     pass
     return(oxide)
 
-def lower_slab(atoms):
-    """Lowers the slab in the unit cell,
-    so that the bottom layer is at zero.
-    Parameters
-    ----------
-        atoms : object.
-    """
-    z = []
-    for atom in atoms:
-        z.append(atom.position[2])
-    z = sorted(z)
-    for atom in atoms:
-        atoms.positions[atom.index][2] = atoms.positions[atom.index][2] - z[0]
-    return(atoms)
-
-def save_structure(atoms, slab_element=None, facet=None, adsorbate=None, local_path=None):
-    """ Save the structure and automatically find out where.
-
-    Parameters
-    ----------
-        atoms : object
-        slab_element : string of slab name or element
-        facet : Miller indices of surface
-        adsorbate : strin of adsorbate name
-        local path : costumize your folder name
-    """
-    if slab_element and facet and adsorbate:
-        traj = adsorbate+'_'+slab_element+str(facet)+'.json'
-        folder = os.getcwd()+'/'+slab_element+'/'+str(facet)+'/'+adsorbate+'/'
-    elif slab_element and facet and not adsorbate:
-        traj = slab_element+str(facet)+'.json'
-        folder = os.getcwd()+'/'+slab_element+'/'+str(facet)+'/clean/'
-    else:
-       traj = "".join(list(set(atoms.get_chemical_symbols())))+'.json'
-       folder = os.getcwd()+'/'+local_path
-    print(folder)
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-    ase.io.write(folder+traj,atoms)
-    return(atoms)
-
-def setbox(atoms, x=None, y=None, z=21):
-    """Sets the unit cell of the system.
-    Parameters
-    ----------
-        atoms : object.
-    """
-    cell = atoms.get_cell()
-    if not x and not y:
-        atoms.set_cell([cell[0],cell[1],[0,0,z]], scale_atoms=False)
-    else:
-        atoms.set_cell([[x,0,0], [0,y,0],[0,0,z]], scale_atoms=False)
-    return(atoms)
-
 def slab_layers(atoms, max_layers=20, tolerance=0.5):
     """Return a number of layers given a slab.
+
     Parameters
     ----------
-        atoms : object.
-        max_layers : maximum number of layers expected.
-        tolerance : convergence criterion for clustering
-        based on the pooled standard deviation of z-coordinates.
+    atoms : object
+        ASE atoms object.
+    max_layers : maximum number of layers expected.
+    tolerance : convergence criterion for clustering
+    based on the pooled standard deviation of z-coordinates.
+    Suggested : 0.5 for oxides, 0.2 for metals.
+
+    Returns
+    -------
+    layer_avg_z : list
+        List of average z-values of all layers.
+    layer_atoms : list of list
+        Each sublist contains the atom indices of the atoms in that layer.
     """
     oxide = is_oxide(atoms)
     if oxide:
-        stoichiometry = stoichiometry(atoms)
-        elements = list(stoichiometry.keys())[1:]
+        stoi = stoichiometry(atoms)
+        elements = list(stoi.keys())[1:]
         for element in elements:
             if element in metals:
                 indices = [atom.index for atom in atoms if atom.symbol == element]
@@ -201,17 +161,4 @@ def slab_layers(atoms, max_layers=20, tolerance=0.5):
             # print('Found ' + str(n) + ' layers.')
             return(layer_avg_z, layer_atoms)
 
-def stoichiometry(atoms):
-    """Gives the stoichiometry of the system in form of a dictionary.
-    Parameters
-    ----------
-        atoms : object.
-    """
-    symbols = atoms.get_chemical_symbols()
-    num_dict = {}
-    num_dict['total'] = len(symbols)
-    elements = list(set(symbols))
-    for element in elements:
-        count = symbols.count(element)
-        num_dict[element] = count
-    return(num_dict)
+
