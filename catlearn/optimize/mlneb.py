e47e503f3a745b54f2886c93b5f179aa7d437e95
Jose A Garrido Torres <jagt@stanford.edu>
2019-04-25 08:56:31 -0700

    - MLNEB hangs.

--- catlearn/optimize/mlneb.py
+++ catlearn/optimize/mlneb.py
@@ -391,6 +391,8 @@ class MLNEB(object):
                         prev_save_positions.append(i.get_positions())
 
                     neb_opt.run(fmax=(fmax * 0.85), steps=1)
+                    neb_opt.steps = 0
+
                     n_steps_performed += 1
                     get_results_predicted_path(self)
                     unc_ml = np.max(self.uncertainty_path[1:-1])
